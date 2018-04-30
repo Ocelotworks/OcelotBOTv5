5293488fc2584b670160ee2582abbecc69c1f3e6
Neil Trotter <ntrotter699@gmail.com>
2018-04-30 18:41:42 +0100

    Port NumberFact

---
+++ commands/numberfact.js
@@ -0,0 +1,24 @@
+/**
+ * Ported by Neil - 30/04/18
+ */
+const request = require('request');
+module.exports = {
+    name: "Number Fact",
+    usage: "numberfact <number>",
+	
+    commands: ["numberfact"],
+    run: function run(message, args, bot) {
+        if(args.length < 2){
+            message.replyLang("NUMBERFACT_USAGE");
+        }else{
+            request(`http://numbersapi.com/${parseInt(args[1])}/`, function(err, resp, body){
+                if(err){
+					bot.raven.captureException(err);
+					message.replyLang("GENERIC_ERROR");
+				} else {
+					message.channel.send(body);
+				}
+            });
+        }
+    }
+};
