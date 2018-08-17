f56b2102157c8e6d68fdca69c7b6e29301fab921
unacceptableuse <unacceptableuse@gmail.com>
2018-08-17 13:23:02 +0100

    Added ocelotworks !userstats

--- commands/userstats.js
+++ commands/userstats.js
@@ -2,31 +2,147 @@
  * Ported by Neil - 30/04/18
  */
 
+const commonWords = [
+    "was", "is", "the","be","to","of","and","a","in","that","have","i","it","for","not","on","with","he","as","you","do","at","this","but","his","by","from","they","we","say","her","she","or","an","will","my","one","all","would","there","their","what","so","up","out","if","about","who","get","which","go","me","when","make","can","like","time","no","just","him","know","take","person","into","year","your","good","some","could","them","see","other","than","then","now","look","only","come","its","over","think","also","back","after","use","two","how","our","work","first","well","way","even","new","want","because","any","these","give","day","most","us", "it's", "don't", "dont"
+];
+
+const reg = /:[^:\s]+:/g;
+
 module.exports = {
     name: "User Stats",
-	usage: "userstats <user>",
-	commands: ["userstats"],
+    usage: "userstats <user>",
+    commands: ["userstats"],
     categories: ["meta"],
     run: async function run(message, args, bot) {
-	   if(!args[1]){
-			message.replyLang("USERSTATS_NO_USER");
-		}else{
-			const target = args[1].replace(/[<>@!]/g, "");
-			try{
-				message.channel.startTyping();
-				let result = await bot.database.getUserStats(target);
-				if(!result[0]){
-					message.replyLang("USERSTATS_NO_COMMANDS");
-				}else{
-					message.replyLang("USERSTATS_MESSAGE", {target: target, count: result[0].commandCount});
-				}
+        // noinspection EqualityComparisonWithCoercionJS
+        if (message.guild.id == "478950156654346292") {
+            await module.exports.ocelotStats(message, args, bot);
+        } else if (!args[1]) {
+            message.replyLang("USERSTATS_NO_USER");
+        } else {
+            const target = args[1].replace(/[<>@!]/g, "");
+            try {
+                message.channel.startTyping();
+                let result = await bot.database.getUserStats(target);
+                if (!result[0]) {
+                    message.replyLang("USERSTATS_NO_COMMANDS");
+                } else {
+                    message.replyLang("USERSTATS_MESSAGE", {target: target, count: result[0].commandCount});
+                }
 
                 message.channel.stopTyping();
 
-			}catch(e){
-				bot.raven.captureException(e);
-				message.replyLang("GENERIC_ERROR");
-			}
-		}
+            } catch (e) {
+                bot.raven.captureException(e);
+                message.replyLang("GENERIC_ERROR");
+            }
+        }
+    },
+    ocelotStats: async function(message, args, bot){
+        let target = args[1] ? args[1].toLowerCase() : null;
+        if(target && target.startsWith("<")){
+            message.channel.send("You can't mention people, you have to use their actual name i.e !userstats peter");
+            return;
+        }
+        let sentMessage     = await message.channel.send("Generating stats [Getting messages from server]..."),
+            output          = [`**Overview for ${target || "everyone"}**:`],
+            totalChars      = 0,
+            totalWords      = 0,
+            emojis          = {},
+            totalEmojis     = 0,
+            uniqueWords     = {},
+            channels        = {};
+
+
+
+        let messages = await bot.database.getMessages(target);
+
+
+        const totalMessages = messages.length;
+
+        await sentMessage.edit(`Generating stats [Aggregating ${totalMessages.toLocaleString()} messages]...`);
+
+        for(let i = 0; i < totalMessages; i++){
+            const userMessage = messages[i];
+            const messageContent = userMessage.message;
+            const words = messageContent.split(" ");
+
+            totalChars += messageContent.length;
+
+            totalWords += words.length;
+            for(let w = 0; w < words.length; w++){
+                const word = words[w];
+                if(word.length > 3 && commonWords.indexOf(word) === -1){
+                    if(uniqueWords[word]){
+                        uniqueWords[word]++;
+                    }else{
+                        uniqueWords[word] = 1;
+                    }
+                }
+            }
+
+            if(channels[userMessage.channel]){
+                channels[userMessage.channel]++;
+            }else{
+                channels[userMessage.channel] = 1;
+            }
+
+            const emojiRegex = messageContent.match(reg);
+            if(emojiRegex && emojiRegex.length > 0){
+                totalEmojis += emojiRegex.length;
+                for(let e = 0; e < emojiRegex.length; e++){
+                    const emoji = emojiRegex[e];
+                    if(emojis[emoji]){
+                        emojis[emoji]++;
+                    }else{
+                        emojis[emoji] = 1;
+                    }
+                }
+            }
+        }
+
+        await sentMessage.edit(`Generating stats [Sorting]...`);
+
+        const uniqueWordsSorted = Object.keys(uniqueWords).sort(function(a, b) {
+            return uniqueWords[a] - uniqueWords[b]
+        });
+        const channelsSorted = Object.keys(channels).sort(function(a, b) {
+            return channels[a] - channels[b]
+        });
+
+        const emojisSorted = Object.keys(emojis).sort(function(a, b) {
+            return emojis[a] - emojis[b]
+        });
+
+        await sentMessage.edit("Generating stats [Generating output]...");
+
+        setTimeout(async function(){
+            output.push(`- **${totalMessages.toLocaleString()}** total messages.`);
+            output.push(`- **${totalWords.toLocaleString()}** total words. (**${Object.keys(uniqueWords).length.toLocaleString()}** unique)`);
+            output.push(`- **${(totalWords / totalMessages).toFixed(3)}** words per message.`);
+            output.push(`- **${totalChars.toLocaleString()}** total characters.`);
+            output.push(`- At **44** words per minute, this would've taken **${bot.util.prettySeconds((totalWords / 44)*60)}** to type.`);
+            output.push(`- All their messages in a text file would take up **${bot.util.prettyMemory(totalChars)}** of space.`);
+
+            const lastWordIndex = uniqueWordsSorted.length - 1;
+            const lastWord = uniqueWordsSorted[lastWordIndex];
+
+            output.push(`- The most used word is **'${lastWord}'** with **${uniqueWords[lastWord].toLocaleString()}** uses.`);
+
+            const lastEmojiIndex = emojisSorted.length - 1;
+            const lastEmoji = emojisSorted[lastEmojiIndex];
+
+            output.push(`- The most used emoji is **'${lastEmoji}'** with **${emojis[lastEmoji].toLocaleString()}** uses.`);
+            output.push(`They have used **${emojisSorted.length}** different emojis, **${totalEmojis.toLocaleString()}** total times.`);
+
+            const lastChannelIndex = channelsSorted.length - 1;
+            const lastChannel = channelsSorted[lastChannelIndex];
+
+            output.push(`- Their favourite channel is **'${lastChannel}'** with **${channels[lastChannel].toLocaleString()}** messages.`);
+
+
+            await sentMessage.edit(output.join('\n'));
+
+        }, 2000);
     }
 };
