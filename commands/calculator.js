748c138da2c41062239eb5f883eb6893350a628f
unacceptableuse <unacceptableuse@gmail.com>
2018-09-17 17:20:59 +0100

    Add calculator scope and error message

--- commands/calculator.js
+++ commands/calculator.js
@@ -10,6 +10,9 @@ math.import({
     'derivative': function () { throw new Error('Function derivative is disabled') }
 }, {override: true});
 
+
+let scope = {};
+
 module.exports = {
     name: "Calculator",
     usage: "calc [sum]",
@@ -17,11 +20,14 @@ module.exports = {
     requiredPermissions: [],
     commands: ["calc", "calculator"],
     run: async function(message, args, bot){
-        // noinspection EqualityComparisonWithCoercionJS
-        try{
-            message.channel.send(limitedEval(message.content.substring(args[0].length+1)).toString())
-        }catch(e){
-            message.channel.send(e.toString());
+        if(!args[1]){
+            message.channel.send("Usage: !calc [expression]");
+        }else {
+            try {
+                message.channel.send(limitedEval(message.content.substring(args[0].length + 1), scope).toString());
+            } catch (e) {
+                message.channel.send(e.toString());
+            }
         }
     }
 };
