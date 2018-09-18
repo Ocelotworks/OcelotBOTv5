faab449796347388fcd87e93aa881bceb51e0932
unacceptableuse <unacceptableuse@gmail.com>
2018-09-18 17:02:21 +0100

    Added !admin leave

---
+++ commands/admin/leave.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: "Leave Server",
+    usage: "leave <server>",
+    commands: ["leave"],
+    init: function init(bot){
+        if(bot.client.shard){
+            bot.logger.log("Loading shard receiver for !admin leave");
+            process.on("message", function(msg){
+                if(msg.type === "leaveServer"){
+                    const target = msg.message;
+                    if (target) {
+                        bot.logger.log("Leaving " + target.name);
+                        target.leave();
+                    }
+                }
+            });
+        }
+    },
+    run: async function(message, args, bot){
+        bot.client.shard.send({type: "leaveServer", message: args[2]});
+        bot.logger.log("Left Server.");
+    }
+};
