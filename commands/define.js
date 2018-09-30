ce135a997408ab4f009ae4a79a1a8dac3c43dbac
unacceptableuse <unacceptableuse@gmail.com>
2018-09-30 20:02:44 +0100

    Add !define

---
+++ commands/define.js
@@ -0,0 +1,34 @@
+const request = require('request');
+module.exports = {
+    name:  "Dictionary Lookup",
+    usage: "define <word>",
+    categories: ["tools", "fun"],
+    commands: ["define", "def", "dictionary", "dict"],
+    run: function run(message, args, bot) {
+        const term = encodeURIComponent(args.slice(1).join(" "));
+        request("http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword="+term+"&limit=1", function(err, resp, body){
+            if(err){
+                message.channel.send("Error: "+err);
+                bot.raven.captureException(err);
+            }else{
+                try{
+                    const data = JSON.parse(body);
+                    if (data.status !== 200) {
+                        message.channel.send("Error #" + data.status);
+                    } else if (data.count === 0) {
+                        message.channel.send("No definitions found.");
+                    }else if(!data.results[0].senses[0].definition){
+                        message.channel.send("No definition for that word. Be more specific perhaps?");
+                    }else{
+                        const result = data.results[0];
+                        message.channel.send( `*${result.headword}* _${result.part_of_speech}_:\n\`\`\`\n${result.senses[0].definition}\n\`\`\``);
+                    }
+                }catch(e){
+                    message.channel.send("Error: "+err);
+                    bot.raven.captureException(e);
+                }
+            }
+        });
+
+    }
+};
