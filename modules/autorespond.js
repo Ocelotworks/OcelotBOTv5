314f294998023ee07d1872e814e8b9395c4b8f1f
unacceptableuse <unacceptableuse@gmail.com>
2018-10-02 13:20:57 +0100

    Repeat a message thats been repeated 3 times in a row

---
+++ modules/autorespond.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+    name: "Automatic Responses",
+    init: function(bot){
+
+        bot.lastMessages = {};
+        bot.lastMessageCounts = {};
+
+        bot.client.on("message", bot.raven.wrap(async function onMessage(message) {
+            if(message.channel.id === "463607437618970626")return; //What the fuck?
+            if(bot.lastMessages[message.channel.id]){
+                if(bot.lastMessages[message.channel.id] === message.content.toLowerCase()){
+                    if(bot.lastMessageCounts[message.channel.id])
+                        bot.lastMessageCounts[message.channel.id]++;
+                    else
+                        bot.lastMessageCounts[message.channel.id] = 1;
+                    if(bot.lastMessageCounts[message.channel.id] >= 3){
+                        bot.logger.log(`Triggered repeat autorespond at channel ${message.channel.id} from ${message.content} = ${bot.lastMessages[message.channel.id]} ${bot.lastMessageCounts[message.channel.id]} times`)
+                        message.channel.send(bot.lastMessages[message.channel.id]);
+                        bot.lastMessageCounts[message.channel.id] = -1000;
+                    }
+                }else{
+                    bot.lastMessageCounts[message.channel.id] = 0;
+                    bot.lastMessages[message.channel.id] = message.content.toLowerCase();
+                }
+            }else{
+                bot.lastMessages[message.channel.id] = message.content.toLowerCase();
+            }
+        }));
+    }
+};
