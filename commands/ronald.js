b8a7945e0f0ff27320de7a920082f9b643eb3008
unacceptableuse <unacceptableuse@gmail.com>
2018-10-17 21:26:03 +0100

    Add !mcdonald alias for !ronald

--- commands/ronald.js
+++ commands/ronald.js
@@ -3,7 +3,7 @@ module.exports = {
     usage: "ronald <text>",
     rateLimit: 10,
     requiredPermissions: ["ATTACH_FILES"],
-    commands: ["ronald", "ronaldsays"],
+    commands: ["ronald", "ronaldsays", "mcdonald"],
     categories: ["image", "fun", "memes"],
     run:  function(message, args, bot){
         bot.util.processImageMeme(message, args, 207, 77, 15, 15, "ronaldsays.png", "static/ronald.png");
