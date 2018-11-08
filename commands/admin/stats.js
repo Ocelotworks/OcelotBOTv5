6608f01c49a48629bf51a17c167274c3e25c659d
unacceptableuse <unacceptableuse@gmail.com>
2018-11-08 10:28:44 +0000

    Add command processing time to admin stats

--- commands/admin/stats.js
+++ commands/admin/stats.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
+const exec = require('child_process').exec;
 module.exports = {
     name: "Stats",
     usage: "stats",
     commands: ["stats"],
     run: async function(message, args, bot){
+        const now = new Date();
         const stats = await bot.database.getDatabaseStats();
-
         let embed = new Discord.RichEmbed();
         embed.setColor(0x189F06);
         embed.setAuthor("OcelotBOT Admin Stats", "https://cdn.discordapp.com/avatars/146293573422284800/1f37ae7298e956cc7bf671d745ae10ff.png?size=128");
@@ -16,7 +17,17 @@ module.exports = {
         embed.addField("Total Commands Run", stats.commands.toLocaleString(), true);
         embed.addField("Total Messages (This Shard & Session)", bot.stats.messagesTotal, true);
         embed.addField("Total Commands (This Shard & Session)", bot.stats.commandsTotal, true);
+        embed.addField("Shard WS Ping", bot.client.ping+" ms", true);
+        embed.addField("Shard Command Ping", (now-message.createdAt)+" ms", true);
+        embed.addField("Voice Connections/Broadcasts", bot.client.voiceConnections.size+"/"+bot.client.broadcasts.size, true);
 
+        console.log(now);
+        console.log(message.createdAt);
         message.channel.send("", embed);
+
+
+
+
+
     }
 };
