956c4ecbb3803a422553721bef93ad9054c8be29
unacceptableuse <unacceptableuse@gmail.com>
2018-11-27 14:25:13 +0000

    Switch noot and doot over to lang strings

--- commands/noot.js
+++ commands/noot.js
@@ -9,17 +9,17 @@ module.exports = {
     commands: ["noot", "pingu"],
     run:  async function run(message, args, bot){
         if(!message.guild){
-            message.channel.send("This cannot be used in a DM channel.");
+            message.replyLang("GENERIC_DM_CHANNEL");
         }else if(!message.guild.available){
-            message.channel.send("The guild is unavailable due to discord issues. Try agian later.");
+            message.replyLang("GENERIC_GUILD_UNAVAILABLE");
         }else if(!message.member.voiceChannel) {
-            message.channel.send("You need to be in a voice channel to use this command.");
+            message.replyLang("VOICE_NO_CHANNEL");
         }else if(message.member.voiceChannel.full){
-            message.channel.send("That voice channel is full.");
+            message.replyLang("VOICE_FULL_CHANNEL");
         }else if(!message.member.voiceChannel.joinable) {
-            message.channel.send("I don't have permission to join the voice channel you're currently in.");
+            message.replyLang("VOICE_UNJOINABLE_CHANNEL");
         }else if(!message.member.voiceChannel.speakable){
-            message.channel.send("I don't have permission to speak in the voice channel you're currently in.");
+            message.replyLang("VOICE_UNSPEAKABLE_CHANNEL");
         }else{
             try {
                 if(message.guild.voiceConnection) {
@@ -55,5 +55,161 @@ module.exports = {
                 bot.logger.log(e);
             }
         }
+    },
+    test: function(test){
+        test('noot no guild', function(t){
+            const message = {
+                channel: {
+                    send: function(message){
+                        t.is(message, "This cannot be used in a DM channel.")
+                    }
+                }
+            };
+            module.exports.run(message);
+        });
+        test('noot guild unavailable', function(t){
+            const message = {
+                channel: {
+                    send: function(message){
+                        t.is(message, "The guild is unavailable due to discord issues. Try again later.")
+                    }
+                },
+                guild: {
+                    available: false
+                }
+            };
+            module.exports.run(message);
+        });
+        test('noot no voice channel', function(t){
+            const message = {
+                channel: {
+                    send: function(message){
+                        t.is(message, "You need to be in a voice channel to use this command.")
+                    }
+                },
+                guild: {
+                    available: true
+                },
+                member: {}
+            };
+            module.exports.run(message);
+        });
+        test('noot voice channel full', function(t){
+            const message = {
+                channel: {
+                    send: function(message){
+                        t.is(message, "That voice channel is full.")
+                    }
+                },
+                guild: {
+                    available: true
+                },
+                member: {
+                    voiceChannel: {
+                        full: true
+                    }
+                }
+            };
+            module.exports.run(message);
+        });
+        test('noot voice unjoinable', function(t){
+            const message = {
+                channel: {
+                    send: function(message){
+                        t.is(message, "I don't have permission to join the voice channel you're currently in.")
+                    }
+                },
+                guild: {
+                    available: true
+                },
+                member: {
+                    voiceChannel: {
+                        full: false,
+                        joinable: false
+                    }
+                }
+            };
+            module.exports.run(message);
+        });
+        test('noot voice unspeakable', function(t){
+            const message = {
+                channel: {
+                    send: function(message){
+                        t.is(message, "I don't have permission to speak in the voice channel you're currently in.")
+                    }
+                },
+                guild: {
+                    available: true
+                },
+                member: {
+                    voiceChannel: {
+                        full: false,
+                        joinable: true,
+                        speakable: false
+                    }
+                }
+            };
+            module.exports.run(message);
+        });
+        test('noot', function(t){
+            const message = {
+                channel: {
+                    send: function(message){
+                        t.fail();
+                    }
+                },
+                guild: {
+                    available: true,
+                    voiceConnection: {
+                        disconnect: function(){
+                            t.pass();
+                        }
+                    }
+                },
+                member: {
+                    voiceChannel: {
+                        full: false,
+                        joinable: true,
+                        speakable: true,
+                        name: "Channel",
+                        join: function(){
+                            t.pass();
+                            return {
+                                playFile: function(){
+                                    t.pass();
+                                    return {
+                                        on: function(end, callback){
+                                            t.is(end, "end");
+                                            callback();
+                                        }
+                                    }
+                                },
+                                disconnect: function(){
+                                    t.pass();
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            const bot = {
+                logger: {
+                    log: function(message){
+                        console.log(message);
+                    }
+                },
+                util: {
+                    arrayRand: function(array){
+                        return array[0];
+                    }
+                },
+                raven: {
+                    captureException: function(){
+                        t.fail();
+                    }
+                }
+            };
+            module.exports.run(message, null, bot);
+        });
     }
 };
