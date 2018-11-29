30013aaf20a4c848a36ebf5fb0efdfaa4685c25a
unacceptableuse <unacceptableuse@gmail.com>
2018-11-29 16:09:21 +0000

    Add !halloween

---
+++ commands/halloween.js
@@ -0,0 +1,18 @@
+/**
+ *   ╔════   Copyright 2018 Peter Maguire
+ *  ║ ════╗  Created 29/11/2018
+ * ╚════ ║   (ocelotbotv5) halloween
+ *  ════╝
+ */
+const christmas = new Date("31 October 2019");
+module.exports = {
+    name: "Halloween Countdown",
+    usage: "halloween",
+    categories: ["tools", "fun"],
+    commands: ["halloween"],
+    run: function run(message, args, bot) {
+        const diff = (christmas-(new Date()))/1000;
+        message.channel.send(`:ghost: **${bot.util.prettySeconds(diff)}** until halloween!`);
+    }
+
+};
