9af2b66da53798362f4c0c329d2eca208d9223e3
unacceptableuse <unacceptableuse@gmail.com>
2019-02-11 21:48:38 +0000

    fuckin setconfig

--- commands/admin/setConfig.js
+++ commands/admin/setConfig.js
@@ -11,7 +11,7 @@ module.exports = {
     run: async function(message, args, bot){
         const server = args[2] === "this" ? message.guild.id : args[2];
         const key = args[3];
-        const value = message.content.substring(args[0].length+args[1].length+args[2].length+args[3].length+8);
+        const value = message.content.substring(args[0].length+args[1].length+args[2].length+args[3].length+4);
         if(!server || !key){
             message.channel.send("Invalid usage. !admin setconfig server key value");
         }else{
