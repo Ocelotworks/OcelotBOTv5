d43c4ecba93c993b81380a64eee5befcbcc9a2d5
unacceptableuse <unacceptableuse@gmail.com>
2019-02-25 18:26:28 +0000

    Block dodgy searches from !image

--- commands/image.js
+++ commands/image.js
@@ -8,6 +8,11 @@ const GoogleImages = require('google-images');
 const config = require('config').get("Commands.image");
 const Discord = require('discord.js');
 let client = new GoogleImages(config.get("cse"), config.get("key"));
+let cache = {
+    sfw: {},
+    nsfw: {}
+};
+const naughtyRegex = /((sexy|nude|naked)?)( ?)(young( ?)(girl|boy?)|child|kid(die?)|(1?)[0-7]( ?)(year(s?)?)( ?)(old?)|bab(y|ie)|toddler)(s?)( ?)(sexy|porn|sex|naked|nude|fuck(ed?))/gi;
 module.exports = {
     name: "Google Image Search",
     usage: "image <text>",
@@ -18,9 +23,24 @@ module.exports = {
     run:  async function(message, args, bot){
         if(args.length > 1){
             const query = message.cleanContent.substring(args[0].length+1);
+            if(naughtyRegex.test(query)){
+                let embed = new Discord.RichEmbed();
+                embed.setTitle("Search Blocked");
+                embed.setDescription("I'm not going to jail for your edgy joke");
+                embed.setImage("https://i.imgur.com/iHZJOnG.jpg");
+                return message.channel.send("", embed);
+            }
             message.channel.startTyping();
             try {
-                let images = await client.search(query, {safe: message.channel.nsfw ? "off" : "high"});
+                let images;
+                let type = message.channel.nsfw ? "nsfw" : "sfw";
+                if(cache[type][query]) {
+                    bot.logger.log("Using cached copy for "+query);
+                    images = cache[type][query];
+                }else {
+                    images = await client.search(query, {safe: message.channel.nsfw ? "off" : "high"});
+                    cache[type][query] = images;
+                }
                 if(images.length === 0)
                     return message.channel.send(":warning: No results."+(message.channel.nsfw ? " If this is a NSFW search, try it in a NSFW channel." : ""));
 
