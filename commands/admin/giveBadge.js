29ef66f0ee09f27abfbdc167bfc9933c6671fd4c
unacceptableuse <unacceptableuse@gmail.com>
2019-03-13 16:23:09 +0000

    giveBadge uses bot.badges.giveBadge now

--- commands/admin/giveBadge.js
+++ commands/admin/giveBadge.js
@@ -12,13 +12,6 @@ module.exports = {
     run: async function(message, args, bot){
        let id = args[3];
        let user = message.mentions.users.first();
-       await bot.database.giveBadge(user.id, id);
-       const badge = (await bot.database.getBadge(id))[0];
-       let embed = new Discord.RichEmbed();
-       embed.setThumbnail(`https://ocelot.xyz/badge.php?id=${id}`);
-       embed.setTitle(`You just earned ${badge.name}`);
-       embed.setDescription(`${badge.desc}\nNow available on your **${message.getSetting("prefix")}profile**`);
-       embed.setColor("#3ba13b");
-       message.channel.send(user, embed);
+       bot.badges.giveBadge(user, message.channel, id);
     }
 };
