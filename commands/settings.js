e5a0594cc0530ccdb0f6c4e0045cfb525e1c0b2c
unacceptableuse <unacceptableuse@gmail.com>
2019-03-28 12:40:12 +0000

    Fix typo in !settings

--- commands/settings.js
+++ commands/settings.js
@@ -87,7 +87,7 @@ module.exports = {
                     await bot.config.reloadCacheForServer(message.guild.id);
                     message.channel.send(`${bool ? "Enabled" : "Disabled"} serious porn suggestions.`);
                 }else{
-                    message.channel.send(`Try **${args[0]} set seriousporn false** to serious porn suggestions off.`);
+                    message.channel.send(`Try **${args[0]} set seriousporn false** to turn serious porn suggestions off.`);
                 }
             }
         },
@@ -103,12 +103,53 @@ module.exports = {
                 if(args[3] && bot.lang.strings[args[3].toLowerCase()]) {
                     await bot.database.setSetting(message.guild.id, "lang", args[3].toLowerCase());
                     await bot.config.reloadCacheForServer(message.guild.id);
-                    message.channel.send(`Successfully set the language to ${args[3]} (${bot.lang.strings[args[3].toLowerCase()].LANGUAGE_NAME})`);
+                    message.channel.send(`:white_check_mark: Successfully set the language to ${args[3]} (${bot.lang.strings[args[3].toLowerCase()].LANGUAGE_NAME})`);
                 }else{
-                    message.channel.send(`Invalid language. Do ${message.getSetting("prefix")}languages to see the available languages.`);
+                    message.channel.send(`:bangbang: Invalid language. Do ${message.getSetting("prefix")}languages to see the available languages.`);
+                }
+            }
+        },
+        role: {
+            name: "Settings Role",
+            help: "The role needed to use this command.",
+            setting: "settings.role",
+            format: function(input){
+                return `\`${input}\``
+            },
+            onSet: async function(message, args, bot){
+                let roleName = args[3];
+                if(message.mentions.roles.size > 0)
+                    roleName = message.mentions.roles.first().name;
+
+                if(!roleName)
+                    return message.channel.send(":bangbang: You must either enter a role name (e.g bot controller) or @mention the role you want.");
+
+                await bot.database.setSetting(message.guild.id, "settings.role", roleName.toLowerCase());
+                await bot.config.reloadCacheForServer(message.guild.id);
+                message.channel.send(`:white_check_mark: Successfully set the settings role to '${roleName}'.`);
+            }
+        },
+        timezone: {
+            name: "Timezone",
+            help: "The timezone used for !time",
+            setting: "time.zone",
+            format: function(input){
+                return `\`${input}\``
+            },
+            onSet: async function(message, args, bot){
+                let timezone = args[3];
+                if(bot.util.timezones[timezone] || bot.util.timezoneRegex.exec(timezone)){
+                    await bot.database.setSetting(message.guild.id, "time.zone", timezone);
+                    await bot.config.reloadCacheForServer(message.guild.id);
+                    message.channel.send(`:white_check_mark: Successfully set the timezone to '${timezone}'.`);
+                }else{
+                    message.channel.send(":bangbang: Invalid Timezone. Try something like CST or GMT+2");
                 }
             }
         }
+    },
+    init: function(bot){
+        bot.util.standardNestedCommandInit('settings');
     },
     run: async function(message, args, bot){
         if(!message.guild)
@@ -116,46 +157,22 @@ module.exports = {
         if(!message.guild.available)
             return message.replyLang("GENERIC_GUILD_UNAVAILABLE");
 
-
         if(message.guild.ownerID === message.author.id || message.member.roles.find(function(role){
             return role.name.toLowerCase() === "bot master" || role.name.toLowerCase() === message.getSetting("settings.role").toLowerCase();
         })){
-            if(!args[1]){
-                message.channel.send(`:bangbang: Invalid usage. ${args[0]} list/set/help\nOr visit the Dashboard: https://ocelot.xyz/dash/`);
-            }else{
-                let arg =  args[1].toLowerCase();
-                if(arg === "list"){
-                    let output = "```diff\nAvailable Settings:\n";
-                    for(let setting in module.exports.settings){
-                        if(module.exports.settings.hasOwnProperty(setting)) {
-                            let settingInfo = module.exports.settings[setting];
-                            output += `+${settingInfo.name}\n`;
-                            output += `-${settingInfo.help}\n`;
-                            output += ` Set with ${args[0]} set ${setting} ${settingInfo.value || "value"}\n`;
-                            output += "----\n";
-                        }
-                    }
-                    output += "\n```";
-                    message.channel.send(output);
-                }else if(arg === "set"){
-                    if(args[2] && module.exports.settings[args[2].toLowerCase()]){
-                        module.exports.settings[args[2].toLowerCase()].onSet(message, args, bot);
-                    }else{
-                        message.channel.send(`:bangbang: Invalid usage. Try ${args[0]} list`);
-                    }
-                }else if(arg === "help"){
-                    if(args[2] && module.exports.settings[args[2].toLowerCase()]){
-                        const setting = module.exports.settings[args[2].toLowerCase()];
-                        message.channel.send(`${setting.name}:\n${setting.help}`);
-                    }else{
-                        message.channel.send(`:bangbang: You must supply a setting to get help on. Try ${args[0]} list`);
-                    }
+            let arg =  args[1].toLowerCase();
+            if(arg === "help" && args[2]){
+                if(module.exports.settings[args[2].toLowerCase()]){
+                    const setting = module.exports.settings[args[2].toLowerCase()];
+                    message.channel.send(`${setting.name}:\n${setting.help}`);
                 }else{
-                    message.channel.send(`:bangbang: Invalid usage. ${args[0]} list/set/help\nOr visit the Dashboard: https://ocelot.xyz/dash/`)
+                    message.channel.send(`:bangbang: You must supply a valid setting to get help on. Try ${args[0]} list`);
                 }
+            }else {
+                bot.util.standardNestedCommand(message, args, bot, 'settings', module.exports);
             }
         }else{
-            message.channel.send(`:bangbang: You need to have the role '${message.getSetting("settings.role")}' to use this command.`);
+            message.channel.send(`:bangbang: You need to have the role **'${message.getSetting("settings.role")}'* or be the owner of the guild to use this command.`);
         }
     }
 };
