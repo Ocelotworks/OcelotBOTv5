c76cc247c050149eb71c453d54d9913b463e086a
unacceptableuse <unacceptableuse@gmail.com>
2019-04-02 22:31:44 +0100

    Fix prettySeconds showing blank when given values less than a second

--- modules/util.js
+++ modules/util.js
@@ -115,6 +115,7 @@ module.exports = {
          * @returns {String}
          */
         bot.util.prettySeconds = function prettySeconds(seconds) {
+            if(seconds < 1) return "less than a second";
 
             var prettyString = '',
                 data = [];
