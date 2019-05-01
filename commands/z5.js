71f5c52827d8fb1f825e1f445ad018629c669fbf
Neil Trotter <ntrotter699@gmail.com>
2019-05-01 19:48:11 +0100

    First test of Z5 interpreter

---
+++ commands/z5.js
@@ -0,0 +1,26 @@
+var JSZM = require('../jszm.js');
+var fs = require("fs");
+var gameInProgress = false;
+var game;
+
+module.exports = {
+    name: "Z5 Interpreter",
+    usage: "z7",
+    commands: ["z5"],
+    categories: ["fun"],
+    run: async function(message, args, bot){
+
+        if(!gameInProgress){
+            game = new JSZM(fs.readFile("../zdungeon.z5",{}));
+            game.print = function*(text, scripting){
+                message.channel.send(text);
+            };
+
+            game.read = function*(maxlen){
+                return yield args[1];
+            };
+        }
+        gameInProgress = true;
+        game.run();
+    }
+};
