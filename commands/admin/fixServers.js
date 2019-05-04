5ec2def19b9177148ea70638d5808a38c9286a39
unacceptableuse <unacceptableuse@gmail.com>
2019-05-04 18:42:59 +0100

    Delay fixServers to stop the database getting assfucked

--- commands/admin/fixServers.js
+++ commands/admin/fixServers.js
@@ -8,42 +8,52 @@ module.exports = {
     name: "Fix Servers",
     usage: "fixservers",
     commands: ["fixservers"],
-    run: function(message, args, bot){
-        bot.logger.log("Fixing Servers...");
-        const servers = bot.client.guilds;
-        servers.forEach(async function(server){
-            try {
-                let databaseEntry = (await bot.database.getServer(server.id))[0];
-                if (!databaseEntry) {
-                    bot.logger.warn(`${server.name} (${server.id}) does not exist in the database!`);
-                    await bot.database.addServer(server.id, server.ownerID, server.name, server.joinedAt);
-                    databaseEntry = {webhookID: null, webhookToken: null}; //It's called optimisation sweaty look it up
-                } else {
-                    await bot.database.unleaveServer(server.id);
-                    if (databaseEntry.name !== server.name) {
-                        bot.logger.warn(`${server.name} (${server.id}) name is wrong (${databaseEntry.name})`);
-                        await bot.database.updateServer(server.id, {name: server.name, owner: server.ownerID});
-                    }
-                }
+    init: function(bot){
+      process.on("message", function fixServers(message){
+            if(message.type === "fixServers"){
+                let timeout = bot.client.shard.id * 2000;
+                bot.logger.log(`Fixing Servers in ${timeout}ms`);
+                setTimeout(function(){
+                    const servers = bot.client.guilds;
+                    servers.forEach(async function(server){
+                        try {
+                            let databaseEntry = (await bot.database.getServer(server.id))[0];
+                            if (!databaseEntry) {
+                                bot.logger.warn(`${server.name} (${server.id}) does not exist in the database!`);
+                                await bot.database.addServer(server.id, server.ownerID, server.name, server.joinedAt);
+                                databaseEntry = {webhookID: null, webhookToken: null}; //It's called optimisation sweaty look it up
+                            } else {
+                                await bot.database.unleaveServer(server.id);
+                                if (databaseEntry.name !== server.name) {
+                                    bot.logger.warn(`${server.name} (${server.id}) name is wrong (${databaseEntry.name})`);
+                                    await bot.database.updateServer(server.id, {name: server.name, owner: server.ownerID});
+                                }
+                            }
 
-                if (!databaseEntry.webhookID) {
-                    bot.logger.log("Server doesn't have a webhook.");
-                    if (server.me.hasPermission("MANAGE_WEBHOOKS")) {
-                        let mainChannel = bot.util.determineMainChannel(server);
-                        if (mainChannel) {
-                            let webhook = await mainChannel.createWebhook("OcelotBOT", bot.client.avatarURL);
-                            bot.logger.log(`Created webhook for ${server.id}: ${webhook.id}`);
-                            await bot.database.addServerWebhook(server.id, webhook.id, webhook.token);
-                        } else {
-                            bot.logger.warn("Couldn't get a main channel to do it in.")
+                            if (!databaseEntry.webhookID) {
+                                //bot.logger.log("Server doesn't have a webhook.");
+                                if (server.me.hasPermission("MANAGE_WEBHOOKS")) {
+                                    let mainChannel = bot.util.determineMainChannel(server);
+                                    if (mainChannel) {
+                                        let webhook = await mainChannel.createWebhook("OcelotBOT", bot.client.avatarURL);
+                                        bot.logger.log(`Created webhook for ${server.id}: ${webhook.id}`);
+                                        await bot.database.addServerWebhook(server.id, webhook.id, webhook.token);
+                                    } else {
+                                        bot.logger.warn("Couldn't get a main channel to create webhook in.")
+                                    }
+                                } else {
+                                    //bot.logger.warn("No permission to create a webhook.");
+                                }
+                            }
+                        }catch(e){
+                            bot.logger.error(`Failed for server ${server.name} (${server.id}): ${e}`);
                         }
-                    } else {
-                        bot.logger.warn("No permission to create a webhook.");
-                    }
-                }
-            }catch(e){
-                bot.logger.error(`Failed for server ${server.name} (${server.id}): ${e}`);
+                    });
+                }, timeout);
             }
-        });
+      });
+    },
+    run: function(message, args, bot){
+       bot.client.shard.send({type: "fixServers"});
     }
 };
