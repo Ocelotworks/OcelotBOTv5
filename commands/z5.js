701af5556fd08ef9691fd37a8d42b07362d9b302
Neil Trotter <ntrotter699@gmail.com>
2019-05-06 12:57:35 +0100

    Minor code reorganisation

--- commands/z5.js
+++ commands/z5.js
@@ -9,6 +9,74 @@ let gameIterator = {};
 let channel;
 
 
+function startGame(id) {
+    let file = fs.readFileSync("./MINIZORK.Z3", {});
+
+    let game = new JSZM(file);
+    console.log("Created new game");
+
+    let buffer = "";
+    let didPrintHeader = false;
+    game.print = function* (text) {
+        if (text.length === 1 && text === ">") {
+            let channelMessage = "";
+            // split by \n\n to pull out game header, only first time
+            if (!didPrintHeader) {
+                let headerLines = buffer.split("\n\n");
+                channelMessage += "```css\n" + headerLines[0] + "\n```";
+                buffer = headerLines.slice(1).join("\n\n");
+                didPrintHeader = true;
+            }
+
+            // split location and description, then colorize
+            let lines = buffer.split("\n");
+            let location = lines[0];
+            let description = lines.slice(1).join("\n");
+            channelMessage += "```fix\n" + location + "\n```";
+            if (description.replace('\n', '').length > 0) {
+                channelMessage += "```yaml\n" + description + "\n```";
+            }
+            channel.send(channelMessage);
+
+            buffer = "";
+        } else {
+            buffer += text;
+        }
+
+    };
+
+    game.read = function* () {
+        return yield "";
+    };
+
+    game.save = function* (data) {
+        try {
+            yield fs.writeFileSync("./z5saves/" + id, new Buffer(data.buffer), {});
+            return true;
+        } catch (e) {
+            console.log(e);
+            return false;
+        }
+    };
+
+    game.restore = function* () {
+        try {
+            channel.send("Attempting restore");
+            return new Uint8Array(fs.readFileSync("./z5saves/" + id, {}));
+        } catch (e) {
+            console.log(e);
+            channel.send("Restore failed.")
+            return null;
+        }
+
+    };
+
+    gameIterator[id] = game.run();
+    games[id] = game;
+    console.log("Run");
+    gameInProgress[id] = true;
+}
+
 module.exports = {
     name: "Z5 Interpreter",
     usage: "z7",
@@ -113,72 +181,5 @@ module.exports = {
             }
         }*/
 
-        function startGame(id) {
-            let file = fs.readFileSync("./MINIZORK.Z3", {});
-
-            let game = new JSZM(file);
-            console.log("Created new game");
-
-            let buffer = "";
-            let didPrintHeader = false;
-            game.print = function* (text) {
-                if (text.length === 1 && text === ">") {
-                    let channelMessage = "";
-                    // split by \n\n to pull out game header, only first time
-                    if (!didPrintHeader) {
-                        let headerLines = buffer.split("\n\n");
-                        channelMessage += "```css\n" + headerLines[0] + "\n```";
-                        buffer = headerLines.slice(1).join("\n\n");
-                        didPrintHeader = true;
-                    }
-
-                    // split location and description, then colorize
-                    let lines = buffer.split("\n");
-                    let location = lines[0];
-                    let description = lines.slice(1).join("\n");
-                    channelMessage += "```fix\n" + location + "\n```";
-                    if (description.replace('\n', '').length > 0) {
-                        channelMessage += "```yaml\n" + description + "\n```";
-                    }
-                    channel.send(channelMessage);
-
-                    buffer = "";
-                } else {
-                    buffer += text;
-                }
-
-            };
-
-            game.read = function* () {
-                return yield "";
-            };
-
-            game.save = function* (data) {
-                try {
-                    yield fs.writeFileSync("./z5saves/" + id, new Buffer(data.buffer), {});
-                    return true;
-                } catch (e) {
-                    console.log(e);
-                    return false;
-                }
-            };
-
-            game.restore = function* () {
-                try {
-                    channel.send("Attempting restore");
-                    return new Uint8Array(fs.readFileSync("./z5saves/" + id, {}));
-                } catch (e) {
-                    console.log(e);
-                    channel.send("Restore failed.")
-                    return null;
-                }
-
-            };
-
-            gameIterator[id] = game.run();
-            games[id] = game;
-            console.log("Run");
-            gameInProgress[id] = true;
-        }
     }
 };
