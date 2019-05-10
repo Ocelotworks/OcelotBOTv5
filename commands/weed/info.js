75bd2849b71b20f71629d8a9be25fc7f34378bde
Neil Trotter <ntrotter699@gmail.com>
2019-05-10 19:05:19 +0100

    Getting tired of Peter being a sensitive Sally

--- commands/weed/info.js
+++ commands/weed/info.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const util = require('util');
 
 module.exports = {
     name: "Info",
@@ -7,8 +8,14 @@ module.exports = {
     run: async function (message, args, bot, data) {
 
         let authorPlants = data.getPlants()[message.author.id];
+
+        if(authorPlants === undefined || authorPlants.length === 0) {
+            message.channel.send("No plants. Buy some with `!weed buy`");
+            return;
+        }
+
         let pages = authorPlants.chunk(3);
-        let weedbuxString = await bot.lang.getTranslation(message.author.id, "WEED_WEEDBUX", {"weedbux" : data.weedbux[message.author.id]}, message.author.id);
+        let weedbuxString = await bot.lang.getTranslation(message.author.id, "WEED_WEEDBUX", {"weedbux" : message.getSetting("weed.bux")}, message.author.id);
         //console.log(authorPlants[authorPlants.length-1]);
 
         bot.util.standardPagination(message.channel, pages, async function(page, index) {
@@ -17,8 +24,11 @@ module.exports = {
             embed.setAuthor(weedbuxString, bot.client.user.avatarURL);
 
             for(let i = 0; i < page.length; i++){
-                let plant = index * page.length + i;
+                let plant = (index * 3) + i;
                 let value = data.getPlants()[message.author.id][plant];
+                if(value.dead){
+                    break;
+                }
 
                 embed.addField("Plant " + (plant+1), data.status[value.statusIndex][value.age]);
                 console.log("A " + value.waterTime);
@@ -30,6 +40,11 @@ module.exports = {
                     embed.addField(":clock1:", "Ready to harvest", true);
                 }
                 embed.addField(":heart:", value.health + "%", true);
+                if(args[2] === "debug"){
+                    if (!(bot.admins.indexOf(message.author.id) === -1)) {
+                        embed.addField("Debug", util.inspect(value), false);
+                    }
+                }
                 embed.addBlankField(false);
             }
 
--- modules/database.js
+++ modules/database.js
@@ -839,15 +839,16 @@ module.exports = {
                 }
             },
             addNewPlant: async function(plant) {
-                await knex.insert(plant.toStorable()).into("ocelotbot_weed");
-                return knex.select("id").from("ocelotbot_weed").where(plant.toStorable());
+                return await knex.insert(plant.toStorable()).into("ocelotbot_weed").returning('id').then(function(id){
+                    return id;
+                })[0];
             },
             updatePlant: function(plant){
                 return knex("ocelotbot_weed").update(plant.toStorable()).where({id:plant.id}).limit(1);
             },
-            deletePlant: function(plant){
+            deletePlant: async function(plant){
                 //return knex.delete().from("ocelotbot_badge_assignments").where({user: user, badge: badge});
-                return knex().delete().from("ocelotbot_weed").where({id:plant.id});
+                await knex().delete().from("ocelotbot_weed").where({id:plant.id});
             },
             saveAllPlants: function (plantDict) {
                 Object.keys(plantDict).forEach(function (key) {
