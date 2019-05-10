e6e027aca9a5a94c981442ef1dd54d977c30bbc8
unacceptableuse <unacceptableuse@gmail.com>
2019-05-10 16:15:02 +0100

    Add server join/leave subs

---
+++ subscriptions/joins.js
@@ -0,0 +1,27 @@
+/**
+ *   ╔════   Copyright 2019 Peter Maguire
+ *  ║ ════╗  Created 10/05/2019
+ * ╚════ ║   (ocelotbotv5) joins
+ *  ════╝
+ */
+module.exports = {
+    name: "Server Joins",
+    id: "joins",
+    alias: ["serverjoins"],
+    validate: function(input){
+        return null;
+    },
+    added: function added(sub, bot){
+        let message = sub.data ? sub.data : "{{user}}, welcome to {{server}}.";
+        let channel = bot.client.channels.get(sub.channel);
+        bot.client.on("guildMemberAdd", function(guildMember) {
+            if(guildMember.guild.id === sub.server){
+                channel.send(message.formatUnicorn({
+                    user: guildMember.user,
+                    username: guildMember.user.username,
+                    server: guildMember.guild.name
+                }));
+            }
+        });
+    }
+};
---
+++ subscriptions/leaves.js
@@ -0,0 +1,27 @@
+/**
+ *   ╔════   Copyright 2019 Peter Maguire
+ *  ║ ════╗  Created 10/05/2019
+ * ╚════ ║   (ocelotbotv5) leaves
+ *  ════╝
+ */
+module.exports = {
+    name: "Server Leaves",
+    id: "leaves",
+    alias: ["serverleaves"],
+    validate: function(input){
+        return null;
+    },
+    added: function added(sub, bot){
+        let message = sub.data ? sub.data : "{{username}} has left.";
+        let channel = bot.client.channels.get(sub.channel);
+        bot.client.on("guildMemberRemove", function(guildMember) {
+            if(guildMember.guild.id === sub.server){
+                channel.send(message.formatUnicorn({
+                    user: guildMember.user,
+                    username: guildMember.user.username,
+                    server: guildMember.guild.name
+                }));
+            }
+        });
+    }
+};
