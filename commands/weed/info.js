639640712fe79d9364844750faa7e8f13a4749c5
Neil Trotter <ntrotter699@gmail.com>
2019-06-06 10:03:58 +0100

    Tidy up weed info

--- commands/weed/info.js
+++ commands/weed/info.js
@@ -27,38 +27,37 @@ module.exports = {
         });
         bot.database.saveAllPlants(data.getPlants());
 
-        if(authorPlants === undefined || authorPlants.length === 0) {
+        if (authorPlants === undefined || authorPlants.length === 0) {
             message.channel.send("No plants. Buy some with `!weed buy`");
             return;
         }
 
         let pages = authorPlants.chunk(3);
-        let weedbuxString = await bot.lang.getTranslation(message.author.id, "WEED_WEEDBUX", {"weedbux" : message.getSetting("weed.bux")}, message.author.id);
+        let weedbuxString = await bot.lang.getTranslation(message.author.id, "WEED_WEEDBUX", {"weedbux": message.getSetting("weed.bux")}, message.author.id);
         //console.log(authorPlants[authorPlants.length-1]);
 
-        bot.util.standardPagination(message.channel, pages, async function(page, index) {
+        bot.util.standardPagination(message.channel, pages, async function (page, index) {
             let embed = new Discord.RichEmbed();
             embed.setColor(0x189F06);
             embed.setAuthor(weedbuxString, bot.client.user.avatarURL);
             embed.addField("✂ to harvest all harvest-able plants", "💧 to water all plants to full.");
 
-            for(let i = 0; i < page.length; i++){
+            for (let i = 0; i < page.length; i++) {
                 let plant = (index * 3) + i;
                 let value = data.getPlants()[message.author.id][plant];
-                if(value.dead){
+                if (value.dead) {
                     break;
                 }
 
-                embed.addField("Plant " + (plant+1), data.status[value.statusIndex][value.age]);
-                embed.addField(":droplet:", bot.util.prettySeconds((value.waterTime-data.epoch())/1000), true);
-                try {
+                embed.addField("Plant " + (plant + 1), data.status[value.statusIndex][value.age]);
+                embed.addField(":droplet:", bot.util.prettySeconds((value.waterTime - data.epoch()) / 1000), true);
+                if (value.age !== 5) {
                     embed.addField(":clock1:", bot.util.prettySeconds((value.growTime - data.epoch()) / 1000), true);
-                } catch {
-                    //Caught if a plant is ready to harvest, I'll improve this later
+                } else {
                     embed.addField(":clock1:", "Ready to harvest", true);
                 }
                 embed.addField(":heart:", value.health + "%", true);
-                if(args[2] === "debug"){
+                if (args[2] === "debug") {
                     if (!(bot.admins.indexOf(message.author.id) === -1)) {
                         embed.addField("Debug", util.inspect(value), false);
                     }
@@ -67,6 +66,6 @@ module.exports = {
             }
 
             return embed;
-        }, true, 60000, {"💧":data.waterPlants, "✂":data.trimPlants});
+        }, true, 60000, {"💧": data.waterPlants, "✂": data.trimPlants});
     }
 };
