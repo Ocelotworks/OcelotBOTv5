fcbbf0bf39ae847e6368b0e115d7087bfaabb42b
unacceptableuse <unacceptableuse@gmail.com>
2019-09-01 21:38:31 +0100

    Convert noot and doot to lavaplayer

--- commands/doot.js
+++ commands/doot.js
@@ -23,25 +23,7 @@ module.exports = {
             message.replyLang("VOICE_UNSPEAKABLE_CHANNEL");
         }else{
             try {
-                if(message.guild.voiceConnection && message.guild.voiceConnection.channel.id !== message.member.voiceChannel.id && !bot.voiceLeaveTimeouts[message.member.voiceChannel.id])
-                    await message.guild.voiceConnection.disconnect();
-
-
-                if(bot.voiceLeaveTimeouts[message.member.voiceChannel.id])
-                    clearTimeout(bot.voiceLeaveTimeouts[message.member.voiceChannel.id]);
-
                 bot.logger.log("Joining voice channel "+message.member.voiceChannel.name);
-                let connection = await message.member.voiceChannel.join();
-
-                connection.on('error', function(err){
-                    bot.logger.log(err);
-                    message.replyLang("GENERIC_ERROR");
-                });
-
-                connection.on('failed', function(err){
-                    bot.logger.log(err);
-                    message.replyLang("GENERIC_ERROR");
-                });
 
 
                 fs.readdir(__dirname+"/../static/doot", function readDir(err, files){
@@ -49,35 +31,15 @@ module.exports = {
                         bot.logger.log(err);
                         bot.raven.captureException(err);
                         message.replyLang("GENERIC_ERROR");
-                        connection.disconnect();
                     }else{
                         let doot = args[1] && !isNaN(args[1]) ? parseInt(args[1]) : dootCount++ % files.length;
                         if(!files[doot])
                             return message.replyLang("DOOT_NOT_FOUND");
-                        const file = __dirname+"/../static/doot/"+files[doot];
+                        const file = "/home/peter/stevie5/static/doot/"+files[doot];
                         bot.logger.log("Playing "+file);
                         message.replyLang("DOOT", {doot, arg: args[0], fileName: files[doot]});
-                        try {
-                            const dispatcher = connection.playFile(file);
-                            dispatcher.on("end", function fileEnd(){
-                                bot.logger.log("Finished playing");
-                                if(!connection)return;
-                                if(bot.voiceLeaveTimeouts[connection.channel.id])
-                                    clearTimeout(bot.voiceLeaveTimeouts[connection.channel.id]);
-                                bot.voiceLeaveTimeouts[connection.channel.id] = setTimeout(function leaveTimeout(){
-                                    if(connection) {
-                                        bot.logger.log(`Leaving voice channel ${connection.channel.name} (${connection.channel.id})`);
-                                        connection.disconnect();
-                                    }
-                                    delete bot.voiceLeaveTimeouts[connection.channel.id];
-                                }, parseInt(message.getSetting("songguess.leaveTimeout")));
-                            })
-                        }catch(e){
-                            bot.logger.log(e);
-                            bot.raven.captureException(e);
-                            bot.replyLang("GENERIC_ERROR");
-                            connection.disconnect();
-                        }
+                        bot.lavaqueue.playOneSong(message.member.voiceChannel, file);
+
                     }
                 })
             }catch(e){
--- commands/noot.js
+++ commands/noot.js
@@ -24,58 +24,22 @@ module.exports = {
             message.replyLang("VOICE_UNSPEAKABLE_CHANNEL");
         }else{
             try {
-                if(message.guild.voiceConnection && message.guild.voiceConnection.channel.id !== message.member.voiceChannel.id && !bot.voiceLeaveTimeouts[message.member.voiceChannel.id])
-                    await message.guild.voiceConnection.disconnect();
-
-                if(bot.voiceLeaveTimeouts[message.member.voiceChannel.id])
-                    clearTimeout(bot.voiceLeaveTimeouts[message.member.voiceChannel.id]);
-
                 bot.logger.log("Joining voice channel "+message.member.voiceChannel.name);
-                let connection = await message.member.voiceChannel.join();
-
-                connection.on('error', function(err){
-                    bot.logger.log(err);
-                    message.replyLang("GENERIC_ERROR");
-                });
-
-                connection.on('failed', function(err){
-                    bot.logger.log(err);
-                    message.replyLang("GENERIC_ERROR");
-                });
 
                 fs.readdir(__dirname+"/../static/noot", function readDir(err, files){
                     if(err){
                         bot.logger.log(err);
                         bot.raven.captureException(err);
                         message.channel.send("An error occurred. Try again later.");
-                        connection.disconnect();
+
                     }else{
                         let noot = args[1] && !isNaN(args[1]) ? parseInt(args[1]) : nootCount++ % files.length;
                         if(!files[noot])
                             return message.channel.send("No such noot.");
-                        const file = __dirname+"/../static/noot/"+files[noot];
+                        const file = "/home/peter/stevie5/static/noot/"+files[noot];
                         message.channel.send(`<:noot:524657747757891615> Noot #${noot} (${files[noot]})\nUse \`${args[0]} ${noot}\` to play this again.`);
                         bot.logger.log("Playing "+file);
-                        try {
-                            const dispatcher = connection.playFile(file);
-                            dispatcher.on("end", function fileEnd(){
-                                bot.logger.log("Finished playing");
-                                if(bot.voiceLeaveTimeouts[connection.channel.id])
-                                    clearTimeout(bot.voiceLeaveTimeouts[connection.channel.id]);
-                                bot.voiceLeaveTimeouts[connection.channel.id] = setTimeout(function leaveTimeout(){
-                                    if(connection) {
-                                        bot.logger.log(`Leaving voice channel ${connection.channel.name} (${connection.channel.id})`);
-                                        connection.disconnect();
-                                    }
-                                    delete bot.voiceLeaveTimeouts[connection.channel.id];
-                                }, parseInt(message.getSetting("songguess.leaveTimeout")));
-                            })
-                        }catch(e){
-                            bot.logger.log(e);
-                            bot.raven.captureException(e);
-                            message.replyLang("GENERIC_ERROR");
-                            connection.disconnect();
-                        }
+                        bot.lavaqueue.playOneSong(message.member.voiceChannel, file);
                     }
                 })
             }catch(e){
