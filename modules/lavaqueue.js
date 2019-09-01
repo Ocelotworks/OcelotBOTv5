6e384b6bc384a2fb2ddef592cbb4dec95cf60fac
unacceptableuse <unacceptableuse@gmail.com>
2019-09-01 21:39:02 +0100

    Fix lavaplayer integration

--- modules/lavaqueue.js
+++ modules/lavaqueue.js
@@ -4,77 +4,82 @@
  * ╚════ ║   (ocelotbotv5) lavaqueue
  *  ════╝
  */
-const {Client: Lavaqueue} = require('lavaqueue');
-const Redis = require("ioredis");
+const {PlayerManager} = require("discord.js-lavalink");
 const config = require('config');
+const request = require('request');
 module.exports = {
     name: "LavaQueue",
     init: function (bot) {
-        bot.lavaqueue = new Lavaqueue({
-            userID: config.get("Lavalink.user"),
-            password: config.get("Lavalink.password"),
-            shardCount: bot.client.shard.count,
-            hosts: {
-                rest: config.get("Lavalink.rest"),
-                ws: config.get("Lavalink.ws"),
-                redis: new Redis(config.get("Redis.host"))
-            },
-            send: function(guildID, packet){
-                console.log("Got packet", packet);
-                console.log(bot.client.ws.connection);
-                bot.client.ws.connection.ws.send(JSON.stringify(packet));
-                return Promise.resolve();
+        bot.client.on("ready", function(){
+            bot.lavaqueue.manager = new PlayerManager(bot.client,  [{host: "boywanders.us", port: 2333, password: config.get("Lavalink.password")}], {user: bot.client.user.id, shards: bot.client.shard.count});
+        });
+
+        bot.lavaqueue = {};
+        bot.lavaqueue.getSongs = async function getSongs(search) {
+            return new Promise(function(fulfill, reject){
+                const node = bot.lavaqueue.manager.nodes.first();
+
+                const params = new URLSearchParams();
+                params.append("identifier", search);
+                request({url: `http://${node.host}:${node.port}/loadtracks?${params.toString()}`,  headers: { Authorization: node.password } }, function(err, resp, body){
+                    if(err)
+                        return reject(err);
+                    console.log(body);
+                    try {
+                        let data = JSON.parse(body);
+                        if (data.tracks)
+                            fulfill(data.tracks);
+                        else
+                            fulfill();
+
+                    }catch(e){
+                        reject(e);
+                    }
+                })
+            });
+        };
+
+        bot.lavaqueue.getSong = async function getSong(search) {
+           return (await bot.lavaqueue.getSongs(search))[0];
+        };
+
+        bot.lavaqueue.leaveTimeouts = [];
+        bot.lavaqueue.requestLeave = function requestLeave(channel){
+            bot.logger.log("Requested leave for "+channel.id);
+           bot.lavaqueue.cancelLeave(channel);
+            bot.lavaqueue.leaveTimeouts[channel.id] = setTimeout(async function leaveVoiceChannel(){
+                bot.logger.log("Leaving voice channel "+channel.id);
+                await bot.lavaqueue.manager.leave(channel.guild.id);
+            }, 15000);
+        };
+        bot.lavaqueue.cancelLeave = function cancelLeave(channel){
+            if(bot.lavaqueue.leaveTimeouts[channel.id]) {
+                bot.logger.log("Leave cancelled bot "+channel.id);
+                clearTimeout(bot.lavaqueue.leaveTimeouts[channel.id]);
             }
-        });
+        };
 
-        console.log(bot.lavaqueue);
-
-        bot.lavaqueue.on('event', (d) => {
-            console.log('track ended!', d);
-        });
-
-       // console.log(bot.client);
-
-        bot.client.ws.on("VOICE_STATE_UPDATE", function voiceStateUpdate(state){
-            bot.logger.log("Voice state update"+state);
-
-        });
-
-        bot.client.on("raw", function(packet){
-            switch(packet.t){
-                case "VOICE_SERVER_UPDATE":
-                    bot.lavaqueue.voiceServerUpdate(packet.d);
-                    break;
-                case "VOICE_STATE_UPDATE":
-                    bot.lavaqueue.voiceStateUpdate(packet.d);
-                    break;
-                case "GUILD_CREATE":
-                    for (const state of packet.d.voice_states) bot.lavaqueue.voiceStateUpdate(state);
-                    break;
-            }
-        });
-
-        bot.client.ws.on("VOICE_SERVER_UPDATE", function voiceServerUpdate(info){
-            bot.logger.log("Voice server update"+info);
-            bot.lavaqueue.voiceServerUpdate(info);
-        });
-
-        bot.client.ws.on("GUILD_CREATE", function voiceServerUpdate(guild){
-            bot.logger.log("Guild create"+guild);
-
-        });
-
-        bot.client.on("ready", async function lqTest(){
-            const res = await bot.lavaqueue.load('ytsearch:lofi music');
-            const queue = bot.lavaqueue.queues.get("322032568558026753");
-            await queue.player.join("336995004419407872");
-            console.log(res);
-            await queue.add(...res.tracks.map(s => s.track));
-            console.log("Added");
-            await queue.start();
-            console.log("Started");
-        });
-
-
+        bot.lavaqueue.playOneSong = async function playOneSong(voiceChannel, song){
+            bot.lavaqueue.cancelLeave(voiceChannel);
+            const player = await bot.lavaqueue.manager.join({
+                guild: voiceChannel.guild.id, // Guild id
+                channel:  voiceChannel.id, // Channel id
+                host: "boywanders.us" // lavalink host, based on array of nodes
+            });
+            let songData = await bot.lavaqueue.getSong(song);
+            console.log(songData);
+            player.play(songData.track);
+            player.on("error", function playerError(error){
+                bot.raven.captureException(error);
+                bot.logger.error(error);
+            });
+            player.once("end", data => {
+                if (data.reason === "REPLACED") return; // Ignore REPLACED reason to prevent skip loops
+                console.log(data);
+                bot.logger.log("Song ended");
+                bot.lavaqueue.requestLeave(voiceChannel);
+            });
+            return {songData, player};
+        };
     }
 };
