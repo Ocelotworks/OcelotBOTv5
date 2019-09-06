2ef35ecdac0a72fb25d3e328a2bc1c08c924e5e0
unacceptableuse <unacceptableuse@gmail.com>
2019-09-07 00:30:03 +0100

    Fix !m queue causing an error with no arguments

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -9,45 +9,44 @@ module.exports = {
     usage: "queue <url/next>",
     commands: ["queue", "play", "add", "q"],
     run: async function(message, args, bot, music){
-        if(!args[2]){
-            message.replyLang("MUSIC_QUEUE_USAGE");
-        }else{
-            let query = message.cleanContent.substring(args[0].length+args[1].length+2).trim();
-            let guild = message.guild.id;
-            if(!music.listeners[guild]){
-                if(!message.member.voiceChannel)
-                    return message.channel.send(":warning: You have to be in a voice channel to use this command.");
-                const host = bot.util.arrayRand(message.getSetting("music.host").split(","));
-                bot.logger.log("Using host "+host);
-                const player = await bot.lavaqueue.manager.join({
-                    guild:    message.guild.id,
-                    channel:  message.member.voiceChannel.id,
-                    host
-                }, {selfdeaf: true});
-                music.listeners[guild] = {
-                    connection: player,
-                    voiceChannel: message.member.voiceChannel,
-                    voteSkips: [],
-                    queue: [],
-                    server: guild,
-                    channel: message.channel,
-                    host,
-                    playing: null
-                };
+        if(!args[2])
+            return message.replyLang("MUSIC_QUEUE_USAGE");
 
-            }
-            let song = await music.addToQueue(guild, query, message.author.id);
-            if(music.listeners[guild].queue.length > 0) {
-                if (!song)
-                    return message.channel.send(":warning: No results.");
-                if (song.count)
-                    return message.channel.send(`:white_check_mark: Added **${song.count}** songs from playlist **${song.name}** (${bot.util.prettySeconds(song.duration / 1000)})`);
-                if(song.title.indexOf("-") > -1)
-                    return message.channel.send(`:white_check_mark: Added **${song.title}** to the queue.`);
+        let query = message.cleanContent.substring(args[0].length+args[1].length+2).trim();
+        let guild = message.guild.id;
+        if(!music.listeners[guild]){
+            if(!message.member.voiceChannel)
+                return message.channel.send(":warning: You have to be in a voice channel to use this command.");
+            const host = bot.util.arrayRand(message.getSetting("music.host").split(","));
+            bot.logger.log("Using host "+host);
+            const player = await bot.lavaqueue.manager.join({
+                guild:    message.guild.id,
+                channel:  message.member.voiceChannel.id,
+                host
+            }, {selfdeaf: true});
+            music.listeners[guild] = {
+                connection: player,
+                voiceChannel: message.member.voiceChannel,
+                voteSkips: [],
+                queue: [],
+                server: guild,
+                channel: message.channel,
+                host,
+                playing: null
+            };
 
-                message.channel.send(`:white_check_mark: Added **${song.author} - ${song.title}** to the queue.`);
-            }
         }
 
+        let song = await music.addToQueue(guild, query, message.author.id);
+        if(music.listeners[guild].queue.length > 0) {
+            if (!song)
+                return message.channel.send(":warning: No results.");
+            if (song.count)
+                return message.channel.send(`:white_check_mark: Added **${song.count}** songs from playlist **${song.name}** (${bot.util.prettySeconds(song.duration / 1000)})`);
+            if(song.title.indexOf("-") > -1)
+                return message.channel.send(`:white_check_mark: Added **${song.title}** to the queue.`);
+
+            message.channel.send(`:white_check_mark: Added **${song.author} - ${song.title}** to the queue.`);
+        }
     }
 };
