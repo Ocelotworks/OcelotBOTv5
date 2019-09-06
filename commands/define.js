9d06a681549f72c071df927a0e950c454b1c70e3
Peter Maguire <peter.maguire@lot.to>
2019-09-06 13:31:14 +0100

    Fix !define

--- commands/define.js
+++ commands/define.js
@@ -1,41 +1,40 @@
 const request = require('request');
+const key = require('config').get("Commands.define.key");
+const Discord = require('discord.js');
 module.exports = {
     name:  "Dictionary Lookup",
     usage: "define <word>",
     categories: ["tools", "fun"],
     commands: ["define", "def", "dictionary", "dict"],
     run: function run(message, args, bot) {
-        if(!args[1]){
-            message.channel.send(`Usage: ${args[0]} <term>`);
-            return;
-        }
+        if(!args[1])
+            return message.channel.send(`Usage: ${args[0]} <term>`);
+
         const term = encodeURIComponent(args.slice(1).join(" "));
-        request("http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword="+term, function(err, resp, body){
-            if(err){
-                message.channel.send("Error: "+err);
-                bot.raven.captureException(err);
-            }else{
-                try{
-                    const data = JSON.parse(body);
-                    if (data.status !== 200) {
-                        message.replyLang("DEFINE_ERRORNUM", {num: data.status});
-                    } else if (data.count === 0) {
-                        message.replyLang("DEFINE_NOT_FOUND");
-                    }else if(!data.results[0].senses){
-                        message.replyLang("DEFINE_NO_SENSES");
-                    }else if(!data.results[0].senses[0].definition){
-                        message.replyLang("DEFINE_NO_DEFINITION");
-                    }else{
-                        bot.util.standardPagination(message.channel, data.results, async function(page){
-                            return `*${page.headword}* _${page.part_of_speech ? page.part_of_speech : "Thing"}_:\n\`\`\`\n${page.senses[0] ? page.senses[0].definition : ":thinking: Word exists but has no definition."}\n\`\`\``
-                        }, true);
-                    }
-                }catch(e){
-                    message.channel.send("Error: "+err);
-                    bot.raven.captureException(e);
-                }
-            }
+
+        request({uri:"https://wordsapiv1.p.mashape.com/words/"+encodeURIComponent(term), headers:{"X-Mashape-Key": key}}, function(err, resp, body){
+           if(err)
+               return message.replyLang("GENERIC_ERROR");
+
+           try{
+               const data = JSON.parse(body);
+               bot.util.standardPagination(message.channel, data.results, async function(result){
+                   const embed = new Discord.RichEmbed();
+
+                   embed.setTitle(`Definition for "${data.word}" (${result.partOfSpeech}):`);
+                   embed.setDescription(result.definition);
+                   if(result.synonyms)
+                    embed.addField("Synonyms", result.synonyms.join(", "));
+                   return embed;
+               })
+           }catch(e){
+               bot.raven.captureException(e);
+               message.channel.send("Definition not found.");
+           }
+
+
         });
+
 
     }
 };
