effb91bdb2c647d7be826a43a07291f568029595
unacceptableuse <unacceptableuse@gmail.com>
2019-09-17 21:15:24 +0100

    fix !zoom

--- commands/zoom.js
+++ commands/zoom.js
@@ -6,6 +6,6 @@ module.exports = {
     requiredPermissions: ["ATTACH_FILES"],
     commands: ["zoom", "blur"],
     run: async function(message, args, bot){
-        bot.util.processImageFilter(module, message, args, "motion-blur", [message.getSetting("zoom.radius")]);
+        bot.util.processImageFilter(module, message, args, "motionBlur", [message.getSetting("zoom.radius"), message.getSetting("zoom.sigma")]);
     }
 };
