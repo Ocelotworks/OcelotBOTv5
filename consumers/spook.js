d973dbc168381606a8f75d101ddbe395b219132d
unacceptableuse <unacceptableuse@gmail.com>
2019-09-22 19:35:53 +0100

    Spook consumer

---
+++ consumers/spook.js
@@ -0,0 +1,53 @@
+/**
+ *   ╔════   Copyright 2019 Peter Maguire
+ *  ║ ════╗  Created 22/09/2019
+ * ╚════ ║   (ocelotbotv5) spook
+ *  ════╝
+ */
+process.env["NODE_CONFIG_DIR"] = "../config";
+const   config          = require('config'),
+        amqplib         = require('amqplib'),
+        https           = require('https'),
+        fs              = require('fs'),
+        ws              = require('ws');
+
+
+async function init(){
+
+    const server = https.createServer({
+        key: fs.readFileSync('/home/peter/privkey.pem'),
+        cert: fs.readFileSync('/home/peter/cert.pem')
+    });
+    const wss = new ws.Server({server});
+
+
+    server.listen(8234);
+    let con = await amqplib.connect(config.get("RabbitMQ.host"));
+    let channel = await con.createChannel();
+
+    channel.assertQueue('spook');
+
+    channel.consume('spook', function(msg){
+        const str = msg.content.toString();
+        console.log("Processing "+str);
+        let spook = JSON.parse(msg.content);
+        console.log(spook);
+        wss.clients.forEach(function(client){
+            console.log(client.filter);
+            if(!client.filter || client.filter === spook.server)
+                client.send(str);
+        });
+
+        channel.ack(msg);
+    });
+
+    wss.on('connection', function(ws){
+        console.log("Connection");
+        ws.on('message', function(data){
+            console.log(data);
+            ws.filter = data;
+        });
+    });
+}
+
+init();
