6a4f822ab0cb5cf6095d0c7696b61ac5a2c5ecb1
unacceptableuse <unacceptableuse@gmail.com>
2019-11-02 20:47:39 +0000

    Added !colour

---
+++ commands/colour.js
@@ -0,0 +1,30 @@
+/**
+ *   ╔════   Copyright 2019 Peter Maguire
+ *  ║ ════╗  Created 02/11/2019
+ * ╚════ ║   (ocelotbotv5) colour
+ *  ════╝
+ */
+const Discord = require('discord.js');
+const canvas = require('canvas');
+module.exports = {
+    name: "Colour Code",
+    usage: "colour <code>",
+    detailedUsage: "Accepts HTML Colour codes e.g #FF0000",
+    categories: ["tools"],
+    rateLimit: 40,
+    commands: ["colour", "color"],
+    run: function run(message, args, bot) {
+        if(!args[1]){
+            return message.replyLang("GENERIC_TEXT", {command: args[0]})
+        }else{
+            const size = parseInt(message.getSetting("colour.size"));
+            const cnv = canvas.createCanvas(size, size);
+            const ctx = cnv.getContext("2d");
+            ctx.fillStyle = message.cleanContent.substring(args[0].length+1);
+
+            ctx.fillRect(0,0,cnv.width, cnv.height);
+
+            message.channel.send("", new Discord.Attachment(cnv.toBuffer("image/png"), "colour.png"));
+        }
+    },
+};
