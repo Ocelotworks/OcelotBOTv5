660aaf9cb38a241fb713a275ca773e40ddde4a9f
unacceptableuse <unacceptableuse@gmail.com>
2019-11-03 12:46:42 +0000

    Include bot name in language data

--- modules/lang.js
+++ modules/lang.js
@@ -38,6 +38,7 @@ module.exports = {
         bot.lang.getTranslation = function getTranslation(server, key, format = {}, author){
             return new Promise(async function(fulfill){
                 format.prefix = bot.config.get(server, "prefix", author);
+                format.botName = bot.client.user.username;
                 const langOverride = bot.config.get(server, "lang."+key, author);
 
                 if(bot.config.getBool(server, "lang.debug", author))
