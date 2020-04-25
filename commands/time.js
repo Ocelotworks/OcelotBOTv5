b50efba6115d1c93c149e0ab40f76a4610ef7c7e
Peter Maguire <me@petermaguire.xyz>
2020-04-25 15:34:27 +0100

    this is definitely too many now

--- commands/time.js
+++ commands/time.js
@@ -52,16 +52,23 @@ module.exports = {
             let emoji = `:clock${twelveHourTime}${(time.getMinutes() >= 30) ? "30" : ""}:`;
             if(!message.getBool("wholesome")) {
                 if (time.getHours() === 1 && time.getMinutes() === 15) emoji = "It's a quarter after one I'm all alone and I need you now - ";
-                if (time.getHours() === 3 && time.getMinutes() === 0) emoji = "It's 3AM I'm calling just to tell you that without you here, I'm losing sleep\n";
+                if (time.getHours() === 2 && time.getMinutes() === 0 && time.getSeconds() < 10) emoji = "I'm in the corner skanking  - ";
+                if (twelveHourTime === 2 && time.getMinutes() === 0) emoji = "Be there 2 o'clock at the fountain down the road - ";
+                if (twelveHourTime === 1 && time.getMinutes() === 45) emoji = "It's a quarter to two and I'm done, I'm hanging up on you - ";
+                if (time.getHours() === 3 && time.getMinutes() === 0) emoji = "It's 3AM I'm calling just to tell you that without you here, I'm losing sleep - ";
                 if (twelveHourTime === 4 && time.getMinutes() === 20) emoji = "<:weed:478962396296380422>";
-                if (time.getHours()=== 4 && time.getMinutes() === 30) emoji = "At 4:30 in the morning I'm milking cows, Jedediah feeds the chickens and Jacob plows -";
+                if (time.getHours()=== 4 && time.getMinutes() === 30) emoji = "At 4:30 in the morning I'm milking cows, Jedediah feeds the chickens and Jacob plows - ";
                 if (time.getHours() === 5 && time.getMinutes() === 0) emoji = "It's 5 o'clock in the morning, conversation got boring - ";
-                if (time.getHours() === 7 && time.getMinutes() === 0 && time.getDay() === 5) emoji = "7AM waking up in the morning gotta be fresh gotta go downstairs -";
-                if (time.getHours() === 7 && time.getMinutes() === 45 && time.getDay() === 5) emoji = "7:45 we're driving on the highway -";
+                if (time.getHours() === 7 && time.getMinutes() === 0 && time.getDay() === 5) emoji = "7AM waking up in the morning gotta be fresh gotta go downstairs - ";
+                if (time.getHours() === 7 && time.getMinutes() === 45 && time.getDay() === 5) emoji = "7:45 we're driving on the highway - ";
                 if (twelveHourTime === 9 && time.getMinutes() === 11) emoji = ":airplane: :office: :office:";
+                if (time.getHours() === 19 && time.getMinutes() === 0) emoji = "7 o'clock in the evening watching something stupid on TV - ";
+                if (time.getHours() === 20 && time.getMinutes() === 0) emoji = "It's only 8 o'clock but you're already bored - ";
+                if (time.getHours() === 21 && time.getMinutes() === 0) emoji = "It's 9 in the afternoon, and your eyes are the size of the moon - ";
                 if (time.getHours() === 0 && time.getMinutes() === 15) emoji = "It's a quarter past midnight as we cut through the city -";
                 if (twelveHourTime === 12 && time.getMinutes() === 34) emoji = "🔢";
             }
+
             message.replyLang("TIME_MESSAGE", {
                 time: timeMessage.substring(0, timeMessage.indexOf("GMT")),
                 emoji: emoji
