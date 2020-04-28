39fbe594d0f35b42800993396e5e0e4209857f8a
Peter Maguire <me@petermaguire.xyz>
2020-04-28 20:13:11 +0100

    add bread react topic

--- modules/ocelotworks.js
+++ modules/ocelotworks.js
@@ -1,3 +1,11 @@
+const emojiMaps = {
+    alex: "<:alex:478962386578047000>",
+    joel: "<:joel_high:478962387995852804>",
+    peter: "<:peter:478962397281779713>",
+    neil: "<:neilpoop:478962395692269570>",
+    jake: "<:jake_dino:478962396749103106>"
+};
+
 const userMaps = {
     "145659666687328256": "alex",
     "145193838829371393": "jake",
@@ -6,19 +14,11 @@ const userMaps = {
     "478951521854291988": "holly",
     "145200249005277184": "neil",
     "112386674155122688": "joel",
-    "146293573422284800": "ocelotbot",
-    "483242312206385173": "!imitate",
-    "483242548199030794": "!imitate"
+    "146293573422284800": "ocelotbot"
 };
-const emojiMaps = {
-    alex: "<:alex:478962386578047000>",
-    joel: "<:joel_high:478962387995852804>",
-    peter: "<:peter:478962397281779713>",
-    neil: "<:neilpoop:478962395692269570>",
-    jake: "<:jake_dino:478962396749103106>"
-};
-const ts3 = require('ts3');
-const config = require('config');
+
+//const ts3 = require('ts3');
+//const config = require('config');
 
 const thatsTrue = /.*that('?)s true.*/ig;
 
@@ -58,36 +58,45 @@ module.exports = {
            }
         });
 
+        bot.client.on("messageReactionAdd", async (reaction, user)=>{
+            if(reaction.message.guild.id !== "478950156654346292")return;
+            if(reaction.emoji.toString() !== "🍞")return;
 
-        bot.client.on("ready", function discordReady(){
-            if(bot.client.guilds.has("478950156654346292")) {
-                const notifChannel = bot.client.channels.get("478950156654346294");
-                const ts = new ts3();
-                bot.logger.log("Connecting to TeamSpeak...");
-                ts.connect(config.get("Teamspeak.server"), 10011)
-                    .then(async function connected() {
-                        bot.logger.log("Connected to TeamSpeak");
-                        try {
-                            let auth = await ts.auth(config.get("Teamspeak.user"), config.get("Teamspeak.password"), 1);
-                        } catch (e) {
-                            console.log(e);
-                            bot.logger.log("Auth errored as expected...");
-                        } finally {
-                            ts.setName('OcelotBOT');
-                            ts.subscribe("server");
-                        }
+            await bot.database.addTopic(userMaps[reaction.message.author.id], reaction.message.content);
+            reaction.message.channel.send(`:bread: ${user}: Added _<${userMaps[reaction.message.author.id]}> ${reaction.message.content}_ to the list of topics`);
 
-                    });
-                ts.on('clientJoin', (ev) => {
-                    const user = ev.client.nick;
-                    notifChannel.send(`_${emojiMaps[user.toLowerCase()] || ""}${user} joined TeamSpeak_`);
-                });
-                ts.on('clientLeave', (ev) => {
-                    const user = ev.client.nick;
-                    notifChannel.send(`_${emojiMaps[user.toLowerCase()] || ""}${user} left TeamSpeak_`);
-                });
-            }
-        });
+        })
+
+
+        // bot.client.on("ready", function discordReady(){
+        //     if(bot.client.guilds.has("478950156654346292")) {
+        //         const notifChannel = bot.client.channels.get("478950156654346294");
+        //         const ts = new ts3();
+        //         bot.logger.log("Connecting to TeamSpeak...");
+        //         ts.connect(config.get("Teamspeak.server"), 10011)
+        //             .then(async function connected() {
+        //                 bot.logger.log("Connected to TeamSpeak");
+        //                 try {
+        //                     let auth = await ts.auth(config.get("Teamspeak.user"), config.get("Teamspeak.password"), 1);
+        //                 } catch (e) {
+        //                     console.log(e);
+        //                     bot.logger.log("Auth errored as expected...");
+        //                 } finally {
+        //                     ts.setName('OcelotBOT');
+        //                     ts.subscribe("server");
+        //                 }
+        //
+        //             });
+        //         ts.on('clientJoin', (ev) => {
+        //             const user = ev.client.nick;
+        //             notifChannel.send(`_${emojiMaps[user.toLowerCase()] || ""}${user} joined TeamSpeak_`);
+        //         });
+        //         ts.on('clientLeave', (ev) => {
+        //             const user = ev.client.nick;
+        //             notifChannel.send(`_${emojiMaps[user.toLowerCase()] || ""}${user} left TeamSpeak_`);
+        //         });
+        //     }
+        // });
 
 
     }
