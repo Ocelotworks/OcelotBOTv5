6e1b740a109b8c4da0dd52e42c283a32994d76bc
Peter Maguire <me@petermaguire.xyz>
2020-09-24 19:39:46 +0100

    fix task did not end

--- modules/tasks.js
+++ modules/tasks.js
@@ -19,7 +19,7 @@ module.exports = {
             bot.tasks.taskTimers[name + id] = setTimeout(function taskTimeout() {
                 bot.logger.warn(`Task ${name}-${id} did not end!`);
                 bot.tasks.endTask(name, id);
-            }, 120000);
+            }, 150000);
             bot.logger.info(`Started task ${name} (${id})`);
 
             bot.client.shard.send({type: "tasksClear", payload: false});
