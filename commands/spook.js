760140f69df27b619e3f753e9f1d0f955952ed7f
Peter Maguire <me@petermaguire.xyz>
2020-10-01 23:48:37 +0100

    add apm tracking to spook

--- commands/spook.js
+++ commands/spook.js
@@ -418,7 +418,9 @@ module.exports = {
             return bot.sendSpookEnd(message.guild.id, message.channel, await bot.database.getSpooked(message.guild.id));
 
         if(args.length > 1){
+            let span = bot.apm.startSpan("Check can spookr");
             const canSpook = await bot.database.canSpook(message.author.id, message.guild.id);
+            span.end();
             if (!canSpook)
                 return message.replyLang("SPOOK_UNABLE");
 
@@ -444,17 +446,23 @@ module.exports = {
 
             if(target.id === message.author.id)
                 return message.replyLang("SPOOK_SELF");
+            span = bot.apm.startSpan("Get spook count");
             const result = await bot.database.getSpookCount(target.id, message.guild.id);
+            span.end();
             let count = result[0]['COUNT(*)'] + 1;
             message.replyLang("SPOOK", {
                 count: bot.util.getNumberPrefix(count),
                 spooked: target.id
             });
 
+            span = bot.apm.startSpan("Create spook");
             await bot.spook.createSpook(message.channel, message.author, target);
+            span.end();
         }else{
             const now = new Date();
+            let span = bot.apm.startSpan("Get current spook");
             const result = await bot.database.getSpooked(message.guild.id);
+            span.end();
             if(result[0])
                 return message.replyLang("SPOOK_CURRENT", {spooked: result[0].spooked, time: bot.util.prettySeconds((end-now)/1000), server: message.guild.id});
             message.replyLang("SPOOK_NOBODY");
