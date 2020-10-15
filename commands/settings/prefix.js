2bcfb349edc7d80c51318407f0ea1b1983040b14
Peter Maguire <me@petermaguire.xyz>
2020-10-15 18:16:08 +0100

    add !settings prefix so that people better understand how to change the prefix

---
+++ commands/settings/prefix.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "Prefix Idiotproofing",
+    hidden: true,
+    usage: "prefix",
+    commands: ["prefix", "setprefix", "prefixset"],
+    run: async function(message, args){
+        message.channel.send(`To set the prefix, type **${args[0]} set prefix %**, where % is the prefix that you want.`)
+    }
+};
