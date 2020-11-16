f42952fb341239ebf0050cfa4a71c6f41fdb5f1c
Peter Maguire <me@petermaguire.xyz>
2020-11-16 17:46:50 +0000

    move bigtext rendering into image consumer

--- commands/bigtext.js
+++ commands/bigtext.js
@@ -1,9 +1,4 @@
-const canvas = require('canvas');
-const twemoji = require('twemoji-parser');
 const Discord = require('discord.js');
-const axios = require('axios');
-const gif = require('gifuct-js');
-const CanvasGifEncoder = require('../lib/canvas-gif-encoder');
 module.exports = {
     name: "Big Text Generator",
     usage: "bigtext <text>",
@@ -11,154 +6,37 @@ module.exports = {
     requiredPermissions: ["EMBED_LINKS", "ATTACH_FILES"],
     commands: ["bigtext", "big"],
     run:  async function(message, args, bot) {
+        if (!args[1]) {
+            message.replyLang("GENERIC_TEXT", {command: args[0]});
+            return;
+        }
+        const term = args.slice(1).join(" ");
+        let loadingMessage = await message.channel.send("<a:ocelotload:537722658742337557> Processing...");
+        let response = await bot.rabbit.rpc("imageFilter", {url: term, filter: "bigtext"});
+
+        if(loadingMessage) {
+            await loadingMessage.edit("<a:ocelotload:537722658742337557> Uploading...");
+        }
+
+        if (response.err) {
+            console.log(response);
+            await loadingMessage.delete();
+            return message.channel.send(response.err);
+        }
+
+        let attachment = new Discord.MessageAttachment(Buffer.from(response.image, 'base64'), "bigtext.gif");
         try {
-            if (!args[1]) {
-                message.replyLang("GENERIC_TEXT", {command: args[0]});
-                return;
-            }
-
-            const imageCache = {};
-            const term = args.slice(1).join(" ");
-
-            const regularEmojis = twemoji.parse(term, {assetType: 'png'});
-            const emojiMap = {};
-
-            for (let i = 0; i < regularEmojis.length; i++) {
-                const emoji = regularEmojis[i];
-                emojiMap[emoji.indices[0]] = emoji;
-            }
-
-            const animElements = [];
-
-            const textSize = 64;
-
-            const cnv = canvas.createCanvas(1024, 1024);
-            const ctx = cnv.getContext("2d");
-            ctx.font = textSize + "px Arial";
-            ctx.strokeStyle = "#202225";
-            ctx.fillStyle = "#dcddde";
-
-            let actualWidth = 0;
-            let actualHeight = textSize;
-            let currentWidth = textSize / 2;
-
-
-            for (let i = 0; i < term.length; i++) {
-                const char = term[i];
-                if(char === "\n"){
-                    actualWidth = currentWidth;
-                    currentWidth = textSize / 2;
-                    actualHeight += textSize;
-                    continue;
-                }
-                const emojiHere = emojiMap[i];
-                if (emojiHere) {
-                    const emojiImage = await cacheOrGet(imageCache, emojiHere.url);
-                    ctx.drawImage(emojiImage, currentWidth, actualHeight - (textSize - 2), textSize, textSize);
-                    currentWidth += textSize;
-                    i = emojiHere.indices[1] - 1;
-
-                } else if (char === "<" && i < term.length - 1 && (term[i + 1] === ":" || (i < term.length - 2 && term[i + 1] === "a"))) {
-                    const isStatic = term[i + 1] === ":";
-                    const emojiStartIndex = isStatic ? i + 2 : i + 3;
-                    const emojiStart = term.substring(emojiStartIndex);
-                    const emojiEndIndex = emojiStart.indexOf(">");
-                    const idStart = emojiStart.substring(emojiStart.indexOf(":") + 1, emojiEndIndex);
-                    if (isStatic) {
-                        const emojiImage = await cacheOrGet(imageCache, `https://cdn.discordapp.com/emojis/${idStart}.png?v=1`);
-                        ctx.drawImage(emojiImage, currentWidth, actualHeight - (textSize - 2), textSize, textSize);
-                    } else {
-                        const response = await axios.get(`https://cdn.discordapp.com/emojis/${idStart}.gif?v=1`, {responseType: "arraybuffer"});
-                        const parsedGif = await gif.parseGIF(response.data);
-                        const gifFrames = await gif.decompressFrames(parsedGif, true);
-                        animElements.push({x: currentWidth, y: actualHeight - (textSize - 2), frames: gifFrames});
-                    }
-                    currentWidth += textSize;
-                    i = emojiStartIndex + emojiEndIndex;
-
-                } else {
-                    ctx.fillText(char, currentWidth, actualHeight);
-                    ctx.strokeText(char, currentWidth, actualHeight);
-                    currentWidth += ctx.measureText(char).width;
-                }
-                if (currentWidth >= (1024 - textSize)) {
-                    actualWidth = currentWidth;
-                    currentWidth = textSize / 2;
-                    actualHeight += textSize;
-                } else if (actualWidth < currentWidth) {
-                    actualWidth = currentWidth;
-                }
-            }
-            actualWidth += textSize / 2;
-            actualHeight += textSize / 2;
-
-
-            if (animElements.length > 0) {
-                const encoder = new CanvasGifEncoder(actualWidth, actualHeight);
-
-                let frameCount = 0;
-                for (let i = 0; i < animElements.length; i++) {
-                    if (animElements[i].frames.length > frameCount) {
-                        frameCount = animElements[i].frames.length;
-                        if(frameCount > 100){
-                            frameCount = 100;
-                            break;
-                        }
-                    }
-                }
-
-                console.log("Frame count is ", frameCount);
-
-                let frameTimeTotal = 0;
-                encoder.begin();
-                const newCanvas = canvas.createCanvas(actualWidth, actualHeight);
-                const newCtx = newCanvas.getContext("2d");
-                let finishedFrames = 0;
-                for (let f = 0; f < frameCount; f++) {
-                    console.log("Drawing frame ", f);
-                    newCtx.clearRect(0, 0, actualWidth, actualHeight);
-                    newCtx.drawImage(cnv, 0, 0);
-                    let delayTotal = 0;
-                    for (let i = 0; i < animElements.length; i++) {
-                        let element = animElements[i];
-                        const frame = element.frames[f % element.frames.length];
-                        delayTotal += frame.delay;
-                        const imageCanvas = canvas.createCanvas(frame.dims.width, frame.dims.height);
-                        const imageCtx = imageCanvas.getContext("2d");
-                        let frameData = imageCtx.createImageData(frame.dims.width, frame.dims.height);
-                        frameData.data.set(frame.patch);
-                        imageCtx.putImageData(frameData, frame.dims.left, frame.dims.top);
-                        newCtx.drawImage(imageCanvas, element.x, element.y, textSize, textSize);
-                    }
-                    let frameStart = new Date().getTime();
-                    encoder.addFrame(newCtx, Math.round(delayTotal/animElements.length), f).then(()=>{
-                        finishedFrames++;
-                        frameTimeTotal = (new Date().getTime()-frameStart);
-                        console.log(`Finished frame ${f}, ${finishedFrames} total finished.`);
-                        if(finishedFrames >= frameCount){
-                            console.log("All frames done");
-                            message.channel.send(message.author.id === "139871249567318017" ?
-                                `Frames: ${frameCount}. Total Frame Time: ${frameTimeTotal}ms. Per Frame: ${(frameTimeTotal/frameCount).toFixed(2)}ms/frame` : "",
-                                new Discord.MessageAttachment(encoder.end(), "bigtext.gif"));
-                        }
-                    })
-                }
-            } else {
-                const newCanvas = canvas.createCanvas(actualWidth, actualHeight);
-                const newCtx = newCanvas.getContext("2d");
-                newCtx.drawImage(cnv, 0, 0);
-                message.channel.send("", new Discord.MessageAttachment(newCanvas.toBuffer("image/png"), "bigtext.png"));
+            const performanceMessage = `Frame Count: ${response.performance.frameCount} | Rendering Time: ${bot.util.prettySeconds(response.performance.frameTimeTotal/1000, message.guild ? message.guild.id : "global", message.author.id)} | ${Math.round(response.performance.frameTimeTotal/response.performance.frameCount)}ms/frame`;
+            if(message.author.id === "139871249567318017") {
+                await message.channel.send(performanceMessage,attachment);
+            }else{
+                bot.logger.log(performanceMessage);
+                await message.channel.send(attachment);
             }
         }catch(e){
-            console.log(e);
+            bot.raven.captureException(e);
         }
+        await loadingMessage.delete();
     }
 };
 
-async function cacheOrGet(cache, url){
-    if(cache[url])
-        return cache[url];
-    let result = await canvas.loadImage(url);
-    cache[url] = result;
-    return result;
-}
--- consumers/image.js
+++ consumers/image.js
@@ -9,23 +9,25 @@ const   config          = require('config'),
         amqplib         = require('amqplib'),
         gm              = require('gm'),
         request         = require('@naturalatlas/paranoid-request'),
-        fs              = require('fs');
-        //tracer          = require('dd-trace');
+        fs              = require('fs'),
+        twemoji         = require('twemoji-parser'),
+        axios           = require('axios'),
+        gif             = require('gifuct-js'),
+        CanvasGifEncoder= require('../lib/canvas-gif-encoder'),
+        canvas          = require('canvas');
 
+
+let channel
+
+function reply(msg, payload){
+    channel.ack(msg);
+    console.log("Replying "+msg.properties.replyTo);
+    channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(payload)), {correlationId: msg.properties.correlationId});
+}
 
 async function init(){
-    //tracer.init({
-    //    analytics: true
-    //});
     let con = await amqplib.connect(config.get("RabbitMQ.host"));
-    let channel = await con.createChannel();
-
-
-    function reply(msg, payload){
-        channel.ack(msg);
-        console.log("Replying "+msg.properties.replyTo);
-        channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(payload)), {correlationId: msg.properties.correlationId});
-    }
+    channel = await con.createChannel();
 
     channel.assertQueue('imageFilter');
     channel.prefetch(5);
@@ -34,6 +36,9 @@ async function init(){
         try {
             console.log("Processing " + msg.content.toString());
             let {url, format, filter, input} = JSON.parse(msg.content.toString());
+            if(filter === "bigtext"){
+                return doBigText(msg, url);
+            }
             let fileName = `${__dirname}/../temp/${Math.random()}.png`;
             let shouldProcess = true;
             request({uri: url, timeout: 10000})
@@ -99,3 +104,146 @@ async function init(){
 
 init();
 
+async function doBigText(msg, term){
+    const imageCache = {};
+
+    const regularEmojis = twemoji.parse(term, {assetType: 'png'});
+    const emojiMap = {};
+
+    for (let i = 0; i < regularEmojis.length; i++) {
+        const emoji = regularEmojis[i];
+        emojiMap[emoji.indices[0]] = emoji;
+    }
+
+    const animElements = [];
+
+    const textSize = 64;
+
+    const cnv = canvas.createCanvas(1024, 1024);
+    const ctx = cnv.getContext("2d");
+    ctx.font = textSize + "px Arial";
+    ctx.strokeStyle = "#202225";
+    ctx.fillStyle = "#dcddde";
+
+    let actualWidth = 0;
+    let actualHeight = textSize;
+    let currentWidth = 5;
+
+
+    for (let i = 0; i < term.length; i++) {
+        const char = term[i];
+        if(char === "\n"){
+            actualWidth = currentWidth;
+            currentWidth = 5;
+            actualHeight += textSize;
+            continue;
+        }
+        const emojiHere = emojiMap[i];
+        if (emojiHere) {
+            const emojiImage = await cacheOrGet(imageCache, emojiHere.url);
+            ctx.drawImage(emojiImage, currentWidth, actualHeight - (textSize - 2), textSize, textSize);
+            currentWidth += textSize;
+            i = emojiHere.indices[1] - 1;
+
+        } else if (char === "<" && i < term.length - 1 && (term[i + 1] === ":" || (i < term.length - 2 && term[i + 1] === "a"))) {
+            const isStatic = term[i + 1] === ":";
+            const emojiStartIndex = isStatic ? i + 2 : i + 3;
+            const emojiStart = term.substring(emojiStartIndex);
+            const emojiEndIndex = emojiStart.indexOf(">");
+            const idStart = emojiStart.substring(emojiStart.indexOf(":") + 1, emojiEndIndex);
+            if (isStatic) {
+                const emojiImage = await cacheOrGet(imageCache, `https://cdn.discordapp.com/emojis/${idStart}.png?v=1`);
+                ctx.drawImage(emojiImage, currentWidth, actualHeight - (textSize - 2), textSize, textSize);
+            } else {
+                const response = await axios.get(`https://cdn.discordapp.com/emojis/${idStart}.gif?v=1`, {responseType: "arraybuffer"});
+                const parsedGif = await gif.parseGIF(response.data);
+                const gifFrames = await gif.decompressFrames(parsedGif, true);
+                animElements.push({x: currentWidth, y: actualHeight - (textSize - 2), frames: gifFrames});
+            }
+            currentWidth += textSize;
+            i = emojiStartIndex + emojiEndIndex;
+
+        } else {
+            ctx.fillText(char, currentWidth, actualHeight);
+            ctx.strokeText(char, currentWidth, actualHeight);
+            currentWidth += ctx.measureText(char).width;
+        }
+        if (currentWidth >= (1024 - textSize)) {
+            actualWidth = currentWidth;
+            currentWidth = textSize / 2;
+            actualHeight += textSize;
+        } else if (actualWidth < currentWidth) {
+            actualWidth = currentWidth;
+        }
+    }
+    actualWidth += 5;
+    actualHeight += 5;
+
+
+    if (animElements.length > 0) {
+        const encoder = new CanvasGifEncoder(actualWidth, actualHeight);
+
+        let frameCount = 0;
+        for (let i = 0; i < animElements.length; i++) {
+            if (animElements[i].frames.length > frameCount) {
+                frameCount = animElements[i].frames.length;
+                if(frameCount > 100){
+                    frameCount = 100;
+                    break;
+                }
+            }
+        }
+
+        console.log("Frame count is ", frameCount);
+
+        let frameTimeTotal = 0;
+        encoder.begin();
+        const newCanvas = canvas.createCanvas(actualWidth, actualHeight);
+        const newCtx = newCanvas.getContext("2d");
+        newCtx.imageSmoothingEnabled = false;
+        newCtx.imageSmoothingQuality = "low";
+        newCtx.antialias = "none";
+        newCtx.patternQuality = "nearest";
+        newCtx.quality = "nearest";
+        let finishedFrames = 0;
+        for (let f = 0; f < frameCount; f++) {
+            newCtx.clearRect(0, 0, actualWidth, actualHeight);
+            newCtx.drawImage(cnv, 0, 0);
+            let delayTotal = 0;
+            for (let i = 0; i < animElements.length; i++) {
+                let element = animElements[i];
+                const frame = element.frames[f % element.frames.length];
+                delayTotal += frame.delay;
+                const imageCanvas = canvas.createCanvas(frame.dims.width, frame.dims.height);
+                const imageCtx = imageCanvas.getContext("2d");
+                let frameData = imageCtx.createImageData(frame.dims.width, frame.dims.height);
+                frameData.data.set(frame.patch);
+                imageCtx.putImageData(frameData, frame.dims.left, frame.dims.top);
+                newCtx.drawImage(imageCanvas, element.x, element.y, textSize, textSize);
+            }
+            let frameStart = new Date().getTime();
+            encoder.addFrame(newCtx, Math.round(delayTotal/animElements.length), f).then(()=>{
+                finishedFrames++;
+                frameTimeTotal = (new Date().getTime()-frameStart);
+                console.log(`Finished frame ${f}, ${finishedFrames} total finished.`);
+                if(finishedFrames >= frameCount){
+                    console.log("All frames done");
+                    reply(msg, {image: encoder.end().toString("base64"), performance: {frameCount, frameTimeTotal}})
+                }
+            })
+        }
+    } else {
+        const newCanvas = canvas.createCanvas(actualWidth, actualHeight);
+        const newCtx = newCanvas.getContext("2d");
+        newCtx.drawImage(cnv, 0, 0);
+        reply(msg, {image: newCanvas.toBuffer("image/png").toString("base64")})
+    }
+}
+
+async function cacheOrGet(cache, url){
+    if(cache[url])
+        return cache[url];
+    let result = await canvas.loadImage(url);
+    cache[url] = result;
+    return result;
+}
