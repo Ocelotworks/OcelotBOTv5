a6b267dca125d0e1a1fa2d650304757f1b60b00d
Peter Maguire <me@petermaguire.xyz>
2020-12-13 16:00:25 +0000

    8ball will always give the same response for the same question

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,3 +1,6 @@
+
+const questions = ["am", "are", "is", "will", "does", "can", "should", "r", "czy", "could", "when", "has", "what", "why", "how", "did", "where", "do", "thats", "that's"]
+
 module.exports = {
   name: "Magic 8-ball",
   usage: "8ball <question>",
@@ -5,65 +8,7 @@ module.exports = {
   usageExample: "8ball is OcelotBOT the best bot ever made?",
   categories: ["tools", "fun"],
   commands: ["8ball", "magic8ball"],
-  run: function run(message, args, bot) {
-    message.replyLang(args.length < 2 ? "8BALL_NO_QUESTION" : `8BALL_RESPONSE_${message.getSetting("8ball.rig") || bot.util.intBetween(0,14)}`);
+  run: function run(message, args) {
+    return message.replyLang(args.length < 2 ? "8BALL_NO_QUESTION" : `8BALL_RESPONSE_${message.getSetting("8ball.rig") || (questions.indexOf(args[1])+message.content.length)%14}`);
   },
-  test: function(test){
-      test('8ball with args', function(t){
-          const message = {
-              replyLang: function(message){
-                t.is(message, "8BALL_RESPONSE_0");
-              },
-              getSetting: function(setting){
-                  t.is(setting, "8ball.rig");
-                  return null;
-              }
-          };
-          const args = ["some", "arguments", "here"];
-          const bot = {
-              util: {
-                  intBetween: function(x, y){
-                      return 0;
-                  }
-              }
-          };
-          module.exports.run(message, args, bot);
-      });
-      test('8ball with no args', function(t){
-          const message = {
-              replyLang: function(message){
-                  t.is(message, "8BALL_NO_QUESTION");
-              },
-              getSetting: function(setting){
-                  t.is(setting, "8ball.rig");
-                  return null;
-              }
-
-          };
-          const args = [];
-          module.exports.run(message, args);
-      });
-      test('8ball rigged', function(t){
-          const message = {
-              replyLang: function(message){
-                  t.is(message, "8BALL_RESPONSE_5");
-              },
-              getSetting: function(setting){
-                  t.is(setting, "8ball.rig");
-                  return 5;
-              }
-          };
-          const args = ["some", "arguments", "here"];
-          const bot = {
-              util: {
-                  intBetween: function(x, y){
-                      t.fail();
-                      return 0;
-                  }
-              }
-          };
-          module.exports.run(message, args, bot);
-      });
-
-  }
 };
