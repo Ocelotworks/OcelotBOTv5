0ac0b48c8753b073a32fb22c2ec0f7af7af15f78
Peter Maguire <me@petermaguire.xyz>
2020-12-13 15:06:56 +0000

    fix !admin setpresence

--- commands/admin/setPresence.js
+++ commands/admin/setPresence.js
@@ -5,10 +5,10 @@ module.exports = {
     run:  async function(message, args, bot){
         bot.presenceMessage = args[3] === "clear" ? null : message.content.substring(message.content.indexOf(args[2]));
         if(bot.client.shard){
-            bot.client.shard.send({type: "presence", payload: bot.presenceMessage})
+            return bot.client.shard.send({type: "presence", payload: bot.presenceMessage})
         }else{
             const serverCount   = (await bot.client.shard.fetchClientValues("guilds.size")).reduce((prev, val) => prev + val, 0);
-            bot.client.user.setPresence({
+            return bot.client.user.setPresence({
                 activity: {
                     name: `${bot.presenceMessage && bot.presenceMessage + " | "} ${serverCount} servers.`,
                     type: "LISTENING"
