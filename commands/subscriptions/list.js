dc87814b2b9f98c4baca308cc11b371e9b3f2508
Peter Maguire <me@petermaguire.xyz>
2020-12-20 17:06:13 +0000

    pretty up subs list

--- commands/subscriptions/list.js
+++ commands/subscriptions/list.js
@@ -4,6 +4,7 @@
  * ╚════ ║   (ocelotbotv5) list
  *  ════╝
  */
+const columnify = require('columnify');
 module.exports = {
     name: "List Subs",
     usage: "list",
@@ -11,11 +12,9 @@ module.exports = {
     run:  async function(message, args, bot, data){
         const subs = await bot.database.getSubscriptionsForChannel(message.channel.id);
         if(subs.length > 0){
-            let output = `Active subscriptions for **#${message.channel.name}**:\n`;
-            for(let i = 0; i < subs.length; i++){
-                const sub = subs[i];
-                output += `${sub.id}: ${sub.type} - ${sub.data}\n`
-            }
+            let output = `Active subscriptions for **#${message.channel.name}**:\n\`\`\`\n`;
+            output += columnify(subs.map(({id, type, data})=>({id, type, data})));
+            output += `\n\`\`\`\nTo remove a subscription, type **${args[0]} remove id**`;
             message.channel.send(output);
         }else{
             message.channel.send(`There are no subscriptions in this channel yet! Add one with ${args[0]} add\nor view available subscription types with **${args[0]} types**`);
