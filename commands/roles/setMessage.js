325db15d3b615bb8e1493c888e9e1e0842e2dda4
Peter Maguire <me@petermaguire.xyz>
2020-12-24 19:46:46 +0000

    !roles setmessage now allows you to use a reply to set the message

--- commands/roles/setMessage.js
+++ commands/roles/setMessage.js
@@ -6,7 +6,12 @@ module.exports = {
         const result = await bot.database.addRoleMessage(message.channel.id, message.id);
         const id = result[0];
 
-        roleData[message.guild.id] = {messageID: id, message: message};
+        if(message.reference && message.reference.messageID){
+            roleData[message.guild.id] = {messageID: id, message: await message.channel.messages.fetch(message.reference.messageID)};
+        }else{
+            roleData[message.guild.id] = {messageID: id, message: message};
+        }
+
         message.channel.send("Your message has been set as the role message. Role buttons will be added to that message, feel free to edit it as you please.");
     }
 };
