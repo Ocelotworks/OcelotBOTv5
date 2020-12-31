6ec9eaf4c21321ea339ca1036740c97848061b9c
Peter Maguire <me@petermaguire.xyz>
2020-12-31 14:54:12 +0000

    add !wouldyourather

---
+++ commands/wouldyourather.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: "Would You Rather?",
+    usage: "wouldyourather",
+    detailedHelp: "Gives you a 'would you rather' question.",
+    categories: ["games"],
+    commands: ["wouldyourather", "wyr"],
+    run: async function run(message, args, bot) {
+      let questionResult = await bot.util.getJson("http://either.io/questions/next/1");
+      const question = questionResult.questions[0];
+      let embed = new Discord.MessageEmbed();
+      embed.setTitle("Would You Rather...");
+      embed.setColor("#6dccf2");
+      embed.addField(question.option_1, "OR");
+      embed.addField(question.option_2, question.moreinfo ? `_${question.moreinfo}_` : "　　 　　");
+      message.channel.send(embed);
+    }
+};
