2ef9f5d82c7c5fbf284a54f8590ace55269ff176
Peter Maguire <me@petermaguire.xyz>
2021-01-10 22:44:42 +0000

    add !rainbow

---
+++ commands/rainbow.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: "Rainbow Image",
+    usage: "rainbow <user or url>",
+    rateLimit: 10,
+    categories: ["image", "memes"],
+    requiredPermissions: ["ATTACH_FILES"],
+    commands: ["rainbow", "rainbowimage"],
+    run: async function run(message, args, bot){
+        const url = await bot.util.getImage(message, args);
+        if(!url){
+            message.replyLang("CRUSH_NO_USER");
+            return;
+        }
+        return bot.util.imageProcessor(message, {
+            "components": [
+                {
+                    "url": url,
+                    "filter": [{name: "rainbow"}],
+                },
+            ],
+        }, 'rainbow')
+    }
+};
+
+
