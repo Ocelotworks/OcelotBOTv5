7e5754d2dde0e055e6685f7e9a69d7168cb57cab
Peter Maguire <me@petermaguire.xyz>
2021-01-26 16:35:39 +0000

    temporarily disable the pet command if its conflicting with another bot

--- commands/pet.js
+++ commands/pet.js
@@ -10,7 +10,7 @@ const canvas = require('canvas');
 const size = 112;
 const frames = [{"x":18,"y":18,"w":98,"h":98},{"x":14,"y":30,"w":102,"h":86},{"x":6,"y":36,"w":110,"h":80},{"x":6,"y":30,"w":102,"h":86},{"x":14,"y":18,"w":98,"h":98}];
 
-const ignoredArgs = ["play", "explore", "stats", "walk", "feed", "🍆", "clean"];
+const ignoredArgs = ["play", "explore", "stats", "walk", "feed", "🍆", "clean", "train", "rename"];
 
 module.exports = {
     name: "Pet Pet",
@@ -19,7 +19,13 @@ module.exports = {
     rateLimit: 100,
     commands: ["pet", "petpet"],
     run: async function run(message, args, bot) {
-        if(args[1] && ignoredArgs.includes(args[1].toLowerCase()))return;
+        if(message.guild && !args[1].endsWith("petpet") && args[1] && ignoredArgs.includes(args[1].toLowerCase())){
+            message.channel.send(`:warning: It looks like you're trying to use ${args[0]} with a different bot, I will now temporarily disable the ${args[0]} command on OcelotBOT for you so as not to spam.
+To prevent this in the future, consider changing OcelotBOT's prefix with **${message.getSetting("prefix")}settings set prefix** or disabling the pet command with **${message.getSetting("prefix")}settings disableCommand pet**.
+You can still access this command with ${message.getSetting("prefix")}petpet`);
+            bot.config.cache[message.guild.id]["pet.disable"] = 1;
+            return
+        }
         let avatarURL = await bot.util.getImage(message, args);
         if(!avatarURL)
             return message.channel.send("You must enter an image URL or mention a user");
