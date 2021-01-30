9753b81b47daa655a391726f8fbf6bdd4b31f675
Peter Maguire <me@petermaguire.xyz>
2021-01-30 16:50:14 +0000

    make the healthcheck less upset

--- healthcheck.js
+++ healthcheck.js
@@ -7,14 +7,10 @@ axios.get("http://localhost:8006/stats").then((res)=>{
         process.exit(1);
     }
 
-    if(res.data.messagesPerMinute < 5){
+    if(res.data.messagesPerMinute < (process.env.HEALTHCHECK_MESSAGES_PER_MINUTE || 5)){
         console.error("Messages per minute: ", res.data.messagesPerMinute)
         process.exit(1);
     }
-
-
-
-
 }).catch((err)=>{
     console.error(err);
     process.exit(1);
