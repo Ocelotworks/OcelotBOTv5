22b7137c9923b777687a7705eb57ea506cd3c3a8
Peter Maguire <me@petermaguire.xyz>
2021-01-30 18:07:37 +0000

    send metrics in openmetrics format

--- modules/api.js
+++ modules/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const os = require("os");
 module.exports = {
     name: "HTTP API",
     init: async function (bot) {
@@ -10,6 +11,20 @@ module.exports = {
                 totalShards: process.env.SHARD_COUNT,
             });
         });
+
+
+        bot.api.get("/metrics", (req, res)=>{
+            let output = "";
+            const labels = `{shard=${bot.util.shard},hostname=${os.hostname()}}`
+            for(let key in bot.stats){
+                if(bot.stats.hasOwnProperty(key)){
+                    output += `${key}${labels} ${bot.stats[key]}\n`
+                }
+            }
+            output += `wsPing${labels} ${bot.client.ws.shards.first().ping}\n`;
+            output += `wsStatus${labels} ${bot.client.ws.shards.first().status}\n`
+            res.send(output);
+        })
 
         bot.api.listen(8006, function listen(){
             bot.logger.log("Listening on port 8006");
