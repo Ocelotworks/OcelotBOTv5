46de729f06f080074efc093b82022fd982dae823
Peter Maguire <me@petermaguire.xyz>
2021-01-31 23:25:31 +0000

    update getconfig

--- commands/admin/config.js
+++ commands/admin/config.js
@@ -1,9 +1,16 @@
 const config = require('config');
 module.exports = {
-    name: "Config",
+    name: "Get Config",
     usage: "config <key>",
-    commands: ["config"],
+    commands: ["getconfig"],
     run:  function(message, args, bot){
-        message.channel.send(`\`${config.get(args[2])}\``);
+        let output = `Config Property: \`${args[2]}\`\n`;
+        if(bot.config.cache[message.author.id])
+            output += `**User**: \`${bot.config.cache[message.author.id][args[2]] || "Unset"}\`\n`
+        if(bot.config.cache[message.guild.id])
+            output += `**Guild**: \`${bot.config.cache[message.guild.id][args[2]] || "Unset"}\`\n`
+        if(bot.config.cache.global)
+            output += `**Global**: \`${bot.config.cache.global[args[2]] || "Unset"}\`\n`
+        message.channel.send(output);
     }
 };
