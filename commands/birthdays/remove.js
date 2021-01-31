a913d3335be2e1078a5930951c78ff36d1efdc38
Peter Maguire <me@petermaguire.xyz>
2021-01-31 23:11:00 +0000

    fix accidentally removing your own birthday

--- commands/birthdays/remove.js
+++ commands/birthdays/remove.js
@@ -12,6 +12,9 @@ module.exports = {
         let target = message.author;
         if(message.mentions.users.size > 0)
             target = message.mentions.users.first();
+        else if(args.length > 2){
+            message.channel.send("Invalid user, please @ the user you want to target.");
+        }
         await bot.database.removeBirthday(target.id, message.guild.id);
         message.replyLang("BIRTHDAY_REMOVE_SUCCESS");
     }
