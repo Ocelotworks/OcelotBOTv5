385d29b823490ea2141283f81ecd546de3a48fc7
Peter Maguire <me@petermaguire.xyz>
2021-01-31 23:25:45 +0000

    add remove birthday by search

--- commands/birthdays/remove.js
+++ commands/birthdays/remove.js
@@ -13,9 +13,23 @@ module.exports = {
         if(message.mentions.users.size > 0)
             target = message.mentions.users.first();
         else if(args.length > 2){
-            message.channel.send("Invalid user, please @ the user you want to target.");
+            let allBirthdays = await bot.database.getBirthdays(message.guild.id);
+            let found = false;
+            const search = args.slice(2).join(" ").toLowerCase();
+            for(let i = 0; i < allBirthdays.length; i++){
+                let user = await bot.util.getUserInfo(allBirthdays[i].user);
+                if(!user)continue;
+                if(user.username.toLowerCase().includes(search)){
+                    found = true;
+                    target = user;
+                    break;
+                }
+            }
+            if(!found) {
+                return message.channel.send("Couldn't find user, @ them or try a different search");
+            }
         }
         await bot.database.removeBirthday(target.id, message.guild.id);
-        message.replyLang("BIRTHDAY_REMOVE_SUCCESS");
+        return message.replyLang("BIRTHDAY_REMOVE_SUCCESS", {user: target});
     }
 };
