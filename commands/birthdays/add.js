62c03efcf1e6f05e9fac86bfb8d9d81adbabe952
Peter Maguire <me@petermaguire.xyz>
2021-02-17 22:28:31 +0000

    don't code whilst tired folks

--- commands/birthdays/add.js
+++ commands/birthdays/add.js
@@ -16,19 +16,14 @@ module.exports = {
         let date = chrono.parseDate(message.content);
         if(!date)
             return message.replyLang("BIRTHDAY_ADD_DATE", {command: args[0], arg: args[1], user: bot.client.user});
-        try{
-            try {
-                await bot.database.addBirthday(target.id, message.guild.id, date);
-                if(target.username.startsWith("Deleted User ")){
-                    message.channel.send(":warning: Due to a limitation with Discord, your birthday will not show up in the list until you remove 'Deleted User' from your name.");
-                }
-                message.replyLang("BIRTHDAY_ADD_SUCCESS");
-            }catch(e){
-                message.replyLang("BIRTHDAY_ADD_EXISTS", {command: args[0], target});
+        try {
+            await bot.database.addBirthday(target.id, message.guild.id, date);
+            if(target.username.startsWith("Deleted User ")){
+                message.channel.send(":warning: Due to a limitation with Discord, your birthday will not show up in the list until you remove 'Deleted User' from your name.");
             }
-
+            message.replyLang("BIRTHDAY_ADD_SUCCESS");
         }catch(e){
-            console.error(e);
+            message.replyLang("BIRTHDAY_ADD_EXISTS", {command: args[0], target});
         }
     }
 };
