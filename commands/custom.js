d0eed9890d1160c80c4d8a0d095d17dd7a11db57
Peter Maguire <me@petermaguire.xyz>
2021-03-22 17:14:43 +0000

    fix custom commands able to get stuck in an infinite loop of spamming messages

--- commands/custom.js
+++ commands/custom.js
@@ -6,11 +6,11 @@ module.exports = {
     categories: ["meta"],
     commands: ["custom"],
     premium: true,
-    hidden: true,
     init: function init(bot) {
         bot.util.standardNestedCommandInit("custom");
     },
     run: function run(message, args, bot) {
+        if(message.synthetic)return message.channel.send("You can't run this command from within a custom command.");
         if(!message.guild)return message.replyLang("GENERIC_DM_CHANNEL");
         if (!message.getBool("admin") && !message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("You must have the Manage Server permission to use this command.");
         bot.util.standardNestedCommand(message, args, bot, "custom");
