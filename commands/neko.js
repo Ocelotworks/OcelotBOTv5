a3e935b442949e5798829695a154b6f095fe5ad1
Peter Maguire <me@petermaguire.xyz>
2021-04-04 13:46:57 +0100

    lord forgive me

--- commands/neko.js
+++ commands/neko.js
@@ -1,12 +1,12 @@
 const config = require('config');
 module.exports = {
-    name: "Random Lewd Image",
-    usage: "lewd",
-    usageExample: "lewd",
-    commands: ["lewd"],
+    name: "Random Neko Image",
+    usage: "neko",
+    usageExample: "neko",
+    commands: ["neko", "nekopara"],
     categories: ["nsfw"],
     run: async function(message, args, bot){
-        const data = await bot.util.getJson("https://gallery.fluxpoint.dev/api/nsfw/lewd", null, {
+        const data = await bot.util.getJson("https://gallery.fluxpoint.dev/api/nsfw/nekopara", null, {
             "Authorization": config.get("API.fluxpoint.key"),
         });
         return message.channel.send(data.file)
