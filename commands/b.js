9f7b78a9d1ac7b865edf53f41318e41e7f39bc01
Peter Maguire <me@petermaguire.xyz>
2021-04-04 21:41:08 +0100

    fix !b and move it to the image processor

--- commands/b.js
+++ commands/b.js
@@ -1,10 +1,6 @@
-const Discord = require('discord.js'),
-    request = require('request'),
-    config = require('config'),
-    fs = require('fs'),
-    canvas = require('canvas');
+const request = require('request'),
+    config = require('config');
 
-let b;
 module.exports = {
     name: "B-ify",
     usage: "b <url>",
@@ -13,9 +9,6 @@ module.exports = {
     categories: ["image", "filter", "memes"],
     requiredPermissions: ["ATTACH_FILES"],
     commands: ["b", "bify"],
-    init: async function () {
-        b = await canvas.loadImage(__dirname + "/../static/b.png");
-    },
     run: async function (message, args, bot) {
         const url = await bot.util.getImage(message, args);
         if (!url) {
@@ -44,7 +37,8 @@ module.exports = {
             form: {
                 url: url,
                 apikey: config.get("API.ocr.key"),
-                isOverlayRequired: true
+                isOverlayRequired: true,
+                OCREngine: 2,
             }
         }, async function OCRResponse(err, resp, body) {
             if (err) {
@@ -58,6 +52,11 @@ module.exports = {
                 }
             } else {
                 try {
+                    const payload = {
+                        "components": [
+                            {"url": url}
+                        ]
+                    };
                     let positions = [];
                     const data = JSON.parse(body);
                     if (!data.ParsedResults) {
@@ -74,6 +73,16 @@ module.exports = {
                                 const word = line.Words[w];
                                 const text = word.WordText.toLowerCase();
                                 if (bot.util.vowels.indexOf(text.substring(1, 2)) > -1) {
+                                    payload.components.push({
+                                        url: "b.png",
+                                        local: true,
+                                        pos: {
+                                            x: word.Left-(word.Height/2),
+                                            y: word.Top,
+                                            w: word.Height,
+                                            h: word.Height,
+                                        }
+                                    })
                                     positions.push(word);
                                 }
                             }
@@ -81,28 +90,19 @@ module.exports = {
 
                         if (positions.length === 0) {
                             bot.logger.log("Couldn't find any vowels, just putting a B at the start of the sentence");
-                            positions.push(lines[0].Words[0]);
-                        }
-                        //const filePath = "temp/"+encodeURIComponent(url);
-
-                        const image = await canvas.loadImage(url);
-
-
-                        const canv = canvas.createCanvas(image.width, image.height);
-                        const ctx = canv.getContext("2d");
-
-
-                        ctx.drawImage(image, 0, 0);
-
-                        for (let i = 0; i < positions.length; i++) {
-                            const word = positions[i];
-                            ctx.drawImage(b, word.Left, word.Top, word.Height, word.Height);
+                            payload.components.push({
+                                url: "b.png",
+                                local: true,
+                                pos: {
+                                    x: lines[0].Words[0].Left-(word.Height/2),
+                                    y: lines[0].Words[0].Top,
+                                    w: lines[0].Words[0].Height,
+                                    h: lines[0].Words[0].Height,
+                                }
+                            })
                         }
 
-                        const buff = canv.toBuffer("image/png");
-                        bot.logger.log("Got buffer");
-                        const attachment = new Discord.MessageAttachment(buff, "b.png");
-                        message.channel.send("", attachment);
+                        return bot.util.imageProcessor(message, payload, "eyes");
                     } else {
                         message.replyLang("B_NO_TEXT");
                     }
