d5f0a6c4df2e5e408179e8fad3c6630cc83ee9e8
Peter Maguire <me@petermaguire.xyz>
2021-04-05 14:43:04 +0100

    update !custom docs

--- commands/custom/docs.js
+++ commands/custom/docs.js
@@ -6,8 +6,8 @@ module.exports = {
     run: async function (message, args, bot) {
         const embed = new Discord.MessageEmbed();
         embed.setTitle(`Custom Functions`);
-        embed.setDescription(`Custom Functions are written in [Lua](http://www.lua.org/docs.html) and must return a string, which will be sent as a message.`);
-        embed.addField("Taking Input", `Information about the message is available to you. The structure is as follows:\n\`\`\`json\n${JSON.stringify(bot.util.serialiseMessage(message), null, 1)}\n\`\`\`\nYou can access it in lua through \`message\`.`);
+        embed.setDescription(`Custom Functions are written in [Lua](http://www.lua.org/docs.html) and can optionally return a string, which will be sent as a message.`);
+        embed.addField("Documentation", `You can view advanced documentation [here](https://docs.ocelotbot.xyz/custom-commands)`);
         embed.addField("Example", `A simple example which mentions the user who did the command:\n\`\`\`lua\nreturn "Hello, <@"..message.author.id..">"\n\`\`\``)
         return message.channel.send(embed);
     }
