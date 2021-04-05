d270c128566926cbe563441ac17762d069314eef
Peter Maguire <me@petermaguire.xyz>
2021-04-05 11:55:14 +0100

    flip multiple coins with !flip

--- commands/flip.js
+++ commands/flip.js
@@ -3,14 +3,21 @@
  */
 module.exports = {
     name: "Coin Flip",
-    usage: "flip",
+    usage: "flip [coins]",
     detailedHelp: "Heads or tails",
     usageExample: "flip",
     responseExample: "🌕 TAILS!",
     commands: ["flip", "coin"],
     categories: ["tools"],
     run: function run(message, args, bot) {
-        message.replyLang("FLIP_" + (bot.rigFlip || (Math.random() > 0.5 ? "HEADS" : "TAILS")))
+        if(args[1] && !isNaN(args[1]) && args[1] > 1 && args[1] < 100000){
+            const coins = parseInt(args[1]);
+            let heads = 0;
+            for(let i = 0; i < coins; i++)
+                if(Math.random() > 0.5)heads++;
+            return message.channel.send(`Flipped ${coins.toLocaleString()} coins:\n:full_moon: **${heads.toLocaleString()}** HEADS\n:new_moon: **${(coins-heads).toLocaleString()}** TAILS`);
+        }
+        return message.replyLang("FLIP_" + (bot.rigFlip || (Math.random() > 0.5 ? "HEADS" : "TAILS")))
     },
     test: function (test) {
         test('flip', function (t) {
