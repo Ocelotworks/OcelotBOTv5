247dcf5ae38d1cdcec484fa98ca1f08d0c671512
Peter Maguire <me@petermaguire.xyz>
2021-04-07 23:54:45 +0100

    add colour presets

--- commands/colour.js
+++ commands/colour.js
@@ -4,6 +4,26 @@
  * ╚════ ║   (ocelotbotv5) colour
  *  ════╝
  */
+const presets = {
+    "red": "#ff0000",
+    "green": "#00ff00",
+    "blue": "#0000ff",
+    "white": "#ffffff",
+    "black": "#000000",
+    "yellow": "#ffff00",
+    "cyan": "#00ffff",
+    "magenta": "#ff00ff",
+    "grey": "#c3c3c3",
+    "indigo": "#4B0082",
+    "violet": "#9400D3",
+    "discord": "#7289da",
+    "ocelotbot": "#03f783",
+    "google": ["#4285F4", "#DB4437", "#F4B400", "#0F9D58"],
+    "rainbow": ["#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000"],
+    "gay": ["#E70000","#FF8C00","#FFEF00","#00811F","#0044FF","#760089"],
+    "neapolitan": ["#efa3f5", "#794b4b", "#f5e6ba"],
+    "france": ["#002395", "#ffffff", "#ED2939"]
+}
 module.exports = {
     name: "Colour Code",
     usage: "colour <code> [code] ...",
@@ -20,6 +40,14 @@ module.exports = {
         const colours = [];
         for(let i = 1; i < args.length; i++){
             let input = args[i];
+            const preset = presets[input.toLowerCase()]
+            if(preset){
+                if(typeof preset === "object")
+                    colours.push(...preset);
+                else
+                    colours.push(preset)
+                continue;
+            }
             if(!input.startsWith("#"))input = "#"+input;
             if(input.length !== 7 && input.length !== 9)continue;
             colours.push(input);
