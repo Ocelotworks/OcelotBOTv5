5e4e8a8508978925863cfe5b68d2682adb096e5c
Peter Maguire <me@petermaguire.xyz>
2021-04-11 16:36:50 +0100

    use image processor for !mafia

--- commands/mafia.js
+++ commands/mafia.js
@@ -18,8 +18,6 @@ module.exports = {
         mafiaLogo = await canvas.loadImage(__dirname + "/../static/mafia.png");
     },
     run: async function run(message, args, bot) {
-
-
         if (message.mentions.users.size < 2)
             return message.channel.send(`:bangbang: You must enter 2 users. e.g ${args[0]} ${message.author} ${bot.client.user}`);
 
@@ -29,34 +27,55 @@ module.exports = {
         if (!user1 || !user2)
             return message.channel.send(`:bangbang: You must enter 2 users. e.g ${args[0]} ${message.author} ${bot.client.user}`);
 
-        if (!user1.avatarURL({dynamic: true, format: "png"}) || !user2.avatarURL({dynamic: true, format: "png"}))
-            return message.channel.send("Both users must have an avatar.");
 
-        const avatar1 = await canvas.loadImage(user1.avatarURL({dynamic: true, format: "png"}));
-        const avatar2 = await canvas.loadImage(user2.avatarURL({dynamic: true, format: "png"}));
-
-        const cnv = canvas.createCanvas(680, 460);
-        const ctx = cnv.getContext("2d");
-
-        ctx.drawImage(avatar1, 0, 0, 340, 340);
-        ctx.drawImage(avatar2, 340, 0, 340, 340);
-
-        ctx.font = "40px Sans serif";
-        ctx.fillStyle = "white";
-        ctx.strokeStyle = 'black';
-
-        ctx.fillText("Level 1 Crook", 40, 40);
-        ctx.strokeText("Level 1 Crook", 40, 40);
-
-        ctx.font = "30px Sans serif";
-        const secondText = `Level ${bot.util.intBetween(30, 100)} Mafia Boss`;
-        ctx.fillText(secondText, 350, 40);
-        ctx.strokeText(secondText, 350, 40);
-
-
-        ctx.drawImage(mafiaLogo, 0, 340);
-
-        message.channel.send("", new Discord.MessageAttachment(cnv.toBuffer("image/png"), "mafia.png"));
-
+        return bot.util.imageProcessor(message, {
+            "components": [
+                {
+                    url: user1.avatarURL({dynamic: true, format: "png", size: 512}),
+                    pos: {x: 0, y: 0},
+                    filter: [{
+                        name: "text",
+                        args: {
+                            fontSize: 50,
+                            colour: "#ffffff",
+                            shadowColour: "#000000",
+                            content: "Level 1 Crook",
+                            x: 256,
+                            y: 5,
+                            ax: 0.5,
+                            ay: 0,
+                            w: 512,
+                            spacing: 1.1,
+                            align: 1,
+                        }
+                    }]
+                },{
+                    url: user2.avatarURL({dynamic: true, format: "png", size: 512}),
+                    pos: {x: 512, y: 0},
+                    filter: [{
+                        name: "text",
+                        args: {
+                            fontSize: 50,
+                            colour: "#ffffff",
+                            shadowColour: "#000000",
+                            content: `Level ${bot.util.intBetween(30, 100)} Mafia Boss`,
+                            x: 256,
+                            y: 5,
+                            ax: 0.5,
+                            ay: 0,
+                            w: 512,
+                            spacing: 1.1,
+                            align: 1,
+                        }
+                    }]
+                }, {
+                    url: "mafia.png",
+                    local: true,
+                    pos: {x: 0, y: 512}
+                }
+            ],
+            width: 1024,
+            height: 632,
+        }, 'mafia')
     }
 };
