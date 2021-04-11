4d66db465bceb37ba4342aafb0a230e4c7a5741a
Peter Maguire <me@petermaguire.xyz>
2021-04-11 23:37:10 +0100

    update !monochrome command to use greyscale filter

--- commands/monochrome.js
+++ commands/monochrome.js
@@ -1,15 +1,23 @@
-const Discord = require('discord.js');
-const request = require('request');
-const gm = require('gm');
-const fs = require('fs');
+
 module.exports = {
     name: "Monochrome Image",
     usage: "monochrome [url]",
     rateLimit: 10,
     categories: ["image", "filter"],
     requiredPermissions: ["ATTACH_FILES"],
-    commands: ["monochrome", "blackandwhite"],
+    commands: ["monochrome", "blackandwhite", "greyscale"],
     run: async function(message, args, bot){
-        return bot.util.processImageFilter(module, message, args, "monochrome", []);
+        const url = await bot.util.getImage(message, args);
+        if(!url){
+            return message.replyLang("CRUSH_NO_USER");
+        }
+        return bot.util.imageProcessor(message, {
+            "components": [
+                {
+                    "url": url,
+                    "filter": [{name: "greyscale"}],
+                },
+            ],
+        }, 'greyscale')
     }
 };
