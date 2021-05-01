810d383b3aafea7bc0044712c7a5040f3b7e5b3d
Peter Maguire <me@petermaguire.xyz>
2021-05-01 18:18:57 +0100

    peeepoo

--- commands/custom/view.js
+++ commands/custom/view.js
@@ -12,7 +12,7 @@ module.exports = {
         embed.setDescription(`Use ID ${func.id} to edit/delete this.`);
         embed.addField("Type", func.type, true);
         embed.addField("Trigger", func.trigger, true);
-        if(func.function.length < 1024) {
+        if(func.function.length < 900) {
             embed.addField("Code", `\`\`\`lua\n${func.function}\n\`\`\``);
             return message.channel.send(embed);
         } else {
