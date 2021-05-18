f11971d48263b28720ff13f8c35757c599c9b0da
Peter Maguire <me@petermaguire.xyz>
2021-05-18 20:26:39 +0100

    add !macro

---
+++ commands/macro.js
@@ -0,0 +1,66 @@
+module.exports = {
+    name: "Image Macro",
+    usage: "macro [image] <top text> / <bottom text>",
+    rateLimit: 10,
+    detailedHelp: "Make an image macro meme like it's 2011 again",
+    usageExample: "macro we live in a society / bottom text",
+    requiredPermissions: ["ATTACH_FILES"],
+    commands: ["imagemacro", "macro"],
+    categories: ["image", "memes"],
+    run: async function (message, args, bot) {
+        if (!args[1]) {
+            return message.channel.send(`Enter up to two things like: **${args[0]} top text** or **${args[0]} top text / bottom text**`)
+        }
+        const url = await bot.util.getImage(message, args);
+        if (!url)
+            return message.replyLang("CRUSH_NO_USER");
+        const fullText = message.cleanContent.substring(args[0].length).replace(url, "").toUpperCase();
+        let first = fullText;
+        let second = "";
+        if(fullText.indexOf("/") > -1){
+            const split = fullText.split("/");
+            first = split[0].trim();
+            second = split[1].trim();
+        }
+        return bot.util.imageProcessor(message, {
+            "components": [
+                {
+                    "url": url,
+                    "filter": [{
+                        name: "text",
+                        args: {
+                            font: "impact.ttf",
+                            fontSize: "0.1 * ch",
+                            colour: "#ffffff",
+                            outlineColour: "#000000",
+                            content: first,
+                            x: 0,
+                            y: "0.025 * ch",
+                            ax: 0,
+                            ay: 0,
+                            w: "cw",
+                            spacing: 1.3,
+                            align: 1,
+                        }
+                    }, {
+                        name: "text",
+                        args: {
+                            font: "impact.ttf",
+                            fontSize: "0.1 * ch",
+                            colour: "#ffffff",
+                            outlineColour: "#000000",
+                            content: second,
+                            x: 0,
+                            y: "0.975 * ch",
+                            ax: 0,
+                            ay: 1,
+                            w: "cw",
+                            spacing: 1.3,
+                            align: 1,
+                        }
+                    }]
+                },
+            ]
+        }, "macro")
+    }
+};
