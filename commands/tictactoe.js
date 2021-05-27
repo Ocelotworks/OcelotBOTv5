414c8645aee93a7929001da93eeb5e7193456433
Peter Maguire <me@petermaguire.xyz>
2021-05-27 22:04:28 +0100

    interaction tictactoe

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -1,172 +1,114 @@
-let runningGames = {
-    dummy: {
-        turn: false,
-        players: [1, 2],
-        lastMessage: null,
-        board: [
-            [0,1,0],
-            [null, null, null],
-            [null, 1, null]
-        ]
-    }
-};
-
-let gameRequests = {
-    dummy: {
-        from: 10,
-        to: 10,
-        at: 0
-    }
-};
-
-const cols = ["A", "B", "C"];
-const pieces = ["X", "O", " "];
-
+const Discord = require('discord.js');
 module.exports = {
     name: "Tic Tac Toe",
     usage: "tictactoe start <@player>/<grid position>",
     accessLevel: 0,
     commands: ["tictactoe", "noughtsandcrosses", "ttt"],
     categories: ["games", "fun"],
+    init: function(bot){
+        bot.interactions.addHandler("#", async (interaction)=>{
+            // Get the interaction data and validate that the user is supposed to be clicking this
+            const [turnUser, otherUser, gridPosition, turnType] = interaction.data.custom_id.substring(1).split("|");
+            if(turnUser !== interaction.member.user.id && otherUser !== interaction.member.user.id)
+                return {type: 4, data: {flags: 64, content: "You are not a participant in this game of Tic Tac Toe!"}}
+
+            if(turnUser !== interaction.member.user.id)
+                return {type: 4, data: {flags: 64, content: "It's currently the other players turn."}}
+
+
+            const nextTurn = turnType === "X" ? "O" : "X";
+            const x = Math.floor(gridPosition/3)
+            const y = gridPosition % 3;
+
+            // Set the current grid position
+            interaction.message.components[x].components[y] = {
+                type: 2,
+                style: 2,
+                disabled: true,
+                label: turnType,
+                custom_id: `#${otherUser}|${turnUser}|${gridPosition}|${nextTurn}`
+            }
+
+            let content = `X: <@${turnType === "X" ? turnUser : otherUser}>\nO: <@${turnType === "O" ? turnUser : otherUser}>\n`;
+
+            // Switch the user and turn type for the rest of the buttons
+            interaction.message.components.forEach((c) => c.components.forEach((c) => {
+                console.log(c);
+                const [turnUser, otherUser, gridPosition, turnType] = c.custom_id.substring(1).split("|");
+                c.custom_id = `#${otherUser}|${turnUser}|${gridPosition}|${turnType === "X" ? "O" : "X"}`
+            }))
+
+            const winner = getWin(interaction.message.components);
+            if (winner) {
+                if(winner === "draw"){
+                    content += `:rainbow: It's a draw! Everyone wins.`;
+                }else {
+                    content += `Game over. **${getType(interaction.message.components[winner[0][0]].components[winner[0][1]])} wins!**`
+                }
+                interaction.message.components.forEach((c, x) => c.components.forEach((c, y) => {
+                    c.disabled = true
+                    if(winner !== "draw" && winner.find((a)=>a[0] === x && a[1] === y))c.style = 1;
+                }))
+            }else{
+                content += `Current Turn: ${nextTurn}`
+            }
+
+            // Send the message
+            const channel = await bot.client.channels.fetch(interaction.message.channel_id);
+            let api = new Discord.APIMessage(channel, {});
+            api.data = {content, components: interaction.message.components};
+            let message = await channel.messages.fetch(interaction.message.id);
+            message.edit(api);
+            return {type: 6};
+        })
+    },
     run: function run(message, args, bot){
-        const subCommand = args[1];
-        if(subCommand && module.exports.subCommands[subCommand.toLowerCase()]){
-            module.exports.subCommands[subCommand.toLowerCase()](message, args, bot);
-        }else if(runningGames[message.channel.id] && runningGames[message.channel.id].players[+runningGames[message.channel.id].turn].id === message.author.id) {
-            if(subCommand && subCommand.length === 2 && subCommand.match(/[abc][123]/i)) {
-                const row = cols.indexOf(subCommand[0].toUpperCase());
-                const col = subCommand[1] - 1;
-                module.exports.doGo(message, col, row, bot);
-            }else{
-                message.channel.send(`:bangbang: You must enter a position on the board, e.g ${args[0]} B2`);
-            }
-        }else{
-            message.channel.send(`:bangbang: Invalid usage! To start a game type ${args[0]} start @player`);
+        if(message.mentions?.users?.size == 0)
+            return message.channel.send(`You need to @ a user to play tic tac toe with. e.g **${args[0]} ${message.author}**`);
+        const opponent = message.mentions.users.first();
+        if(opponent.bot)return message.channel.send(`You can't play tic tac toe against a bot.`);
+        const row = (r)=>[
+            {type: 2, style: 2, label: " ", custom_id: `#${message.author.id}|${opponent.id}|${r+0}|X`},
+            {type: 2, style: 2, label: " ", custom_id: `#${message.author.id}|${opponent.id}|${r+1}|X`},
+            {type: 2, style: 2, label: " ", custom_id: `#${message.author.id}|${opponent.id}|${r+2}|X`}
+        ];
+        let api = new Discord.APIMessage(message.channel, {});
+        api.data = {
+            content: `X: ${message.author}\nO: ${opponent}\nCurrent Turn: X`,
+            components: [
+                {type: 1, components: row(0)},
+                {type: 1, components: row(3)},
+                {type: 1, components: row(6),}
+            ]
         }
-
+        return message.channel.send(api);
     },
-    getWinner: function(message){
-        const board = runningGames[message.channel.id].board;
-        for(let i = 0; i < 3; i++){
-            //hoz
-            if(board[i][0] === board[i][1] && board[i][1] === board[i][2])return board[i][0];
-            //vert
-            if(board[0][i] === board[1][i] && board[1][i] === board[2][i])return board[0][i];
-        }
-        //diag
-        if(board[0][0] === board[1][1] && board[1][1] === board[2][2])return board[0][0];
-        if(board[2][0] === board[1][1] && board[1][1] === board[0][2])return board[2][0];
-        return 2;
-    },
-    getDraw: function(message){
-        const board = runningGames[message.channel.id].board;
-        for(let x = 0; x < 3; x++){
-            for(let y = 0; y < 3; y++){
-                if(board[x][y] === 2)return false;
-            }
-        }
-        return true;
-    },
-    doGo: async function(message, row, col, bot){
-        const game = runningGames[message.channel.id];
-        if(game.board[row][col] < 2){
-            message.channel.send(":warning: That space is occupied. Try another space.");
-        }else{
-            game.board[row][col] = +game.turn;
-            const winner = module.exports.getWinner(message);
-            if(winner < 2){
-                const winnerUser = game.players[winner];
-                if(game.lastMessage)
-                    game.lastMessage.delete();
-                message.channel.send(`${winnerUser} wins!\n${module.exports.renderBoard(message.channel.id)}`);
-                bot.tasks.endTask("tictactoe", message.channel.id);
-                delete runningGames[message.channel.id];
-            }else if(module.exports.getDraw(message)){
-                message.channel.send(`Draw! Everyone wins! :rainbow: \n${module.exports.renderBoard(message.channel.id)}`);
-                bot.tasks.endTask("tictactoe", message.channel.id);
-                delete runningGames[message.channel.id];
-            }else{
-                game.turn = !game.turn;
-                if(game.lastMessage)
-                    game.lastMessage.delete();
-                game.lastMessage = await message.channel.send(`${game.players[+game.turn]}, it is your turn:\n${module.exports.renderBoard(message.channel.id)}`);
-            }
-        }
-
-    },
-    renderBoard: function(channel){
-        const game = runningGames[channel];
-        const board =  game.board;
-        const turn = game.turn;
-        let output = "";
-        if(!turn)output += `**${pieces[0]}: ${game.players[0].username}**\n${pieces[1]}: ${game.players[1].username}`;
-        else output += `${pieces[0]}: ${game.players[0].username}\n**${pieces[1]}: ${game.players[1].username}**`;
-        output +=
-`\`\`\`
-╔═══╦═══╦═══╦═══╗
-║   ║ A ║ B ║ C ║
-╠═══╬═══╬═══╬═══╣
-║ 1 ║ ${pieces[board[0][0]]} ║ ${pieces[board[0][1]]} ║ ${pieces[board[0][2]]} ║
-║ 2 ║ ${pieces[board[1][0]]} ║ ${pieces[board[1][1]]} ║ ${pieces[board[1][2]]} ║
-║ 3 ║ ${pieces[board[2][0]]} ║ ${pieces[board[2][1]]} ║ ${pieces[board[2][2]]} ║
-╚═══╩═══╩═══╩═══╝
-\`\`\``;
-
-        return output;
-    },
-    subCommands: {
-        start: function(message, args, bot){
-            const currentGame = runningGames[message.channel.id];
-            const command = args[0];
-            if(currentGame){
-                const authorIndex =currentGame.players.indexOf(message.author.id);
-                if(authorIndex === -1){
-                    message.replyLang("GAME_ALREADY_RUNNING");
-                }else if(authorIndex === currentGame.turn){
-                    message.channel.send(`:warning: It is currently your turn in the current game! Do your move with ${command} <position> i.e ${args[0]} A1 or give up with ${command} quit`);
-                }else{
-                    message.channel.send(`:warning: There is already a game going on in this channel and you are in it. Wait for your opponent to move or give up with ${command} quit`);
-                }
-            }else{
-                if(message.mentions && message.mentions.members && message.mentions.members.size > 0){
-                    const target = message.mentions.members.first();
-                    if(target.user.bot)return message.channel.send(`:bangbang: You cannot challenge a bot to tic tac toe.`);
-                    const now = new Date();
-                    if(!gameRequests[message.channel.id])gameRequests[message.channel.id] = {};
-                    gameRequests[message.channel.id][target.id] = {
-                        at: now,
-                        from: message.author,
-                        to: target
-                    };
-                    message.channel.send(`:rotating_light: ${target}, ${message.author} challenges you to a game of tic tac toe! Type **${args[0]} accept** to start!`);
-                }else{
-                    message.channel.send(`:bangbang: You must mention a user to challenge. ${args[0]} start @user`);
-                }
-            }
-        },
-        accept: async function(message, args, bot){
-            if(gameRequests[message.channel.id] && gameRequests[message.channel.id][message.author.id]){
-                const request = gameRequests[message.channel.id][message.author.id];
-                runningGames[message.channel.id] = {
-                    turn: 0,
-                    players: [request.from, message.author],
-                    board: [[2, 2, 2],[2, 2, 2],[2, 2, 2]],
-                };
-                bot.tasks.startTask("tictactoe", message.channel.id);
-                runningGames[message.channel.id].lastMessage = await message.channel.send(`${request.from}, your request has been accepted! It is your turn.\n${module.exports.renderBoard(message.channel.id)}\nMove with ${args[0]} [position] i.e ${args[0]} B2 for the middle.`);
-                delete gameRequests[message.channel.id];
-            }else{
-                if(gameRequests[message.channel.id]) {
-                    for (let request in gameRequests[message.channel.id]) {
-                        if(gameRequests[message.channel.id].hasOwnProperty(request) && gameRequests[message.channel.id][request].from.id === message.author.id){
-                            message.channel.send(`:bangbang: You are currently waiting for <@${request}> to accept your invite by typing **${args[0]} accept**. To challenge someone else, type **${args[0]} start @user**.`)
-                            return;
-                        }
-                    }
-                }
-                message.channel.send(`:bangbang: You don't currently have any game invites. Type **${args[0]} start @user** to start one yourself.`);
-            }
-        }
-    }
 };
+
+
+function getWin(board){
+    for(let i = 0; i < 3; i++){
+        // Horizontal
+        if(getType(board[i].components[0]) !== " " &&
+            getType(board[i].components[0]) === getType(board[i].components[1]) &&
+            getType(board[i].components[1]) === getType(board[i].components[2]))return [[i,0],[i,1],[i,2]];
+        // Vertical
+        if(getType(board[0].components[i]) !== " " &&
+            getType(board[0].components[i]) === getType(board[1].components[i]) &&
+            getType(board[1].components[i]) === getType(board[2].components[i]))return [[0,i],[1,i],[2,i]];
+    }
+    // Diagonal
+    if(getType(board[0].components[0]) !== " " &&
+        getType(board[0].components[0]) === getType(board[1].components[1]) &&
+        getType(board[1].components[1]) === getType(board[2].components[2]))return [[0,0],[1,1],[2,2]];
+    if(getType(board[2].components[0]) !== " " &&
+        getType(board[2].components[0]) === getType(board[1].components[1]) &&
+        getType(board[1].components[1]) === getType(board[0].components[2]))return [[2,0],[1,1],[0,2]];
+    if(!board.find((c)=>c.components.find((c)=>c.label === " ")))return "draw";
+    return null;
+}
+
+function getType(button){
+    return button.label;
+}
 
--- modules/interactions.js
+++ modules/interactions.js
@@ -6,7 +6,7 @@ module.exports = {
         bot.interactions = {};
 
         bot.interactions.waiting = {};
-
+        bot.interactions.prefix = {};
 
         bot.interactions.addAction = function addAction(text, style, callback){
             const id = uuid();
@@ -14,19 +14,25 @@ module.exports = {
             return bot.util.buttonComponent(text, style, id);
         }
 
+        bot.interactions.addHandler = function addHandler(id, callback){
+            bot.interactions.prefix[id] = callback;
+        }
+
         bot.client.on("raw", async (packet)=>{
             if(packet.t === "INTERACTION_CREATE"){
                 const interaction = packet.d;
                 // Happy path
                 let callback;
-                if(bot.interactions.waiting[interaction.data.custom_id]) {
+                if(interaction.data.custom_id && bot.interactions.prefix[interaction.data.custom_id[0]]){
+                    callback = await bot.interactions.prefix[interaction.data.custom_id[0]](interaction);
+                }else if(bot.interactions.waiting[interaction.data.custom_id]) {
                     callback = {type: 6}
                     bot.interactions.waiting[interaction.data.custom_id](interaction);
                 }else{
                     callback = {type: 4, data: {flags: 64, content: "Sorry, that button is no longer available."}};
                 }
                 await axios.post(`https://discord.com/api/v8/interactions/${interaction.id}/${interaction.token}/callback`, callback);
-                bot.raven.captureBreadcrumb({
+                bot.raven.addBreadcrumb({
                     message: "Interaction",
                     data: interaction,
                 })
