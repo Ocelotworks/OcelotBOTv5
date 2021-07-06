d9ef3318ec8e17ee52466c85729826d8d3fdf0b0
Peter Maguire <me@petermaguire.xyz>
2021-07-06 22:49:57 +0100

    rewrite trivia

---
+++ commands/trivia/categories.js
@@ -0,0 +1,41 @@
+const Embeds = require("../../util/Embeds");
+const {MessageCommandContext} = require("../../util/CommandContext");
+const {axios} = require('../../util/Http');
+module.exports = {
+    name: "Categories",
+    usage: "categories",
+    commands: ["category", "catagory", "categories", "catagories"],
+    run: async function (context, bot) {
+        const embed = new Embeds.AuthorEmbed(context);
+        embed.setTitle("Trivia Categories");
+        embed.setDescription("Select a trivia category from the dropdown below.\nYou can use the ID number for quick access in the future.");
+
+
+        let result = await axios.get("https://opentdb.com/api_category.php");
+        let options = []
+
+        for(let i = 0; i < result.data.trivia_categories.length; i++){
+            const category = result.data.trivia_categories[i];
+            let categoryNames = category.name.split(":")
+            let option = {label: categoryNames[0], description: `ID: ${category.id}`, value: `${category.id}`};
+            if(categoryNames[1]) {
+                option.label = categoryNames[1];
+                option.description = `${categoryNames[0]} (ID: ${category.id})`;
+            }
+
+            options.push(option);
+        }
+
+        let message = await context.send({embeds: [embed], ephemeral: true, components: [bot.util.actionRow(bot.interactions.addDropdown("Select Category...", options, (interaction)=>{
+            const categoryID = parseInt(interaction.data.values[0]);
+            // This is some real funky shit
+            const args = [context.command, categoryID];
+            bot.command.runCommand(bot.command.initContext(new MessageCommandContext(bot, context.message, args, context.command)));
+            if(message)message.delete();
+        }, 1, 1))]});
+
+
+    }
+};
+
+
