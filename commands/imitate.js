bdc8a9e9e4e542ec0ef878b60235ebc911225872
Peter Maguire <me@petermaguire.xyz>
2021-07-25 23:51:58 +0100

    fix(imitate): fix !imitate failing in a channel that follows 10 channels. (Sentry OCELOTBOT-2NZ)

--- commands/imitate.js
+++ commands/imitate.js
@@ -16,19 +16,16 @@ module.exports = {
     categories: ["fun"],
     unwholesome: true,
     slashHidden: true,
+    guildOnly: true,
     handleError: function(context){
         if(!context.options.user)
             return context.sendLang("IMITATE_NO_USER");
         return context.sendLang("IMITATE_NO_MESSAGE");
     },
     run: async function run(context, bot) {
-        if(!context.guild)
-            return context.sendLang({content: "GENERIC_DM_CHANNEL", ephemeral: true});
-
         const target = await context.guild.members.fetch(context.options.user);
         if(!target)
             return context.sendLang({content: "IMITATE_NO_USER", ephemeral: true});
-
 
         if(context.getSetting("imitate.blockedUsers") && context.getSetting("imitate.blockedUsers").indexOf(target.id) > -1)
             return context.sendLang({content: "IMITATE_BLOCKED", ephemeral: true});
@@ -38,12 +35,17 @@ module.exports = {
 
         const webhooks = await context.channel.fetchWebhooks();
         let webhook = webhooks.filter((w)=>w.type === "Incoming" && w.token).first();
+
+        if(!webhook && webhooks.size === 10){
+            return context.sendLang({content: "IMITATE_WEBHOOK_FULL"})
+        }
+
         if(!webhook){
             webhook = await context.channel.createWebhook(bot.client.user.username, bot.client.user.displayAvatarURL({dynamic: true, format: "png"}));
         }
 
         if(!webhook){
-            return context.send({content: "Couldn't find or create a valid webhook.", ephemeral: true});
+            return context.sendLang({content: "IMITATE_WEBHOOK_FAILED", ephemeral: true});
         }
 
         const content = context.options.message;
