cb860adfb75edee1c02a9c5ce9394a4c3e2b9cf7
Peter Maguire <me@petermaguire.xyz>
2021-08-07 15:03:24 +0100

    fix: fix slash commands with no required input options failing when used in wholesome mode

--- commands/settings.js
+++ commands/settings.js
@@ -43,6 +43,7 @@ module.exports = {
                 context.replyLang({content: "GENERIC_WHOLESOME", ephemeral: true});
                 return false;
             }
+            if(context.interaction && !context.interaction.options)return true;
             let content = context.message ? context.message.content : context.interaction.options.map((o)=>o.value).join(" ");
             if (bot.util.swearRegex.exec(content)) {
                 context.reply({content: "No swearing!", ephemeral: true});
