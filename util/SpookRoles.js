308f5e4309c15356e5d94d211b1cfdbb493506ba
Peter Maguire <me@petermaguire.xyz>
2021-10-01 21:16:02 +0100

    fix: don't allow people to get assigned roles for themselves

--- util/SpookRoles.js
+++ util/SpookRoles.js
@@ -14,25 +14,25 @@ module.exports = class SpookRoles {
     }
 
 
-    static async #getEligibleUser(bot, guild, num){
-        return guild.members.fetch().then((ms)=>ms.filter((m)=>!m.user.bot && !bot.config.getBool(guild.id, "spook.optout", m.id) && m.presence && m.presence?.status !== "offline")).then(r=>r.random(num));
+    static async #getEligibleUser(bot, guild, num, user){
+        return guild.members.fetch().then((ms)=>ms.filter((m)=>!m.user.bot && !bot.config.getBool(guild.id, "spook.optout", m.id) && m.presence && m.presence?.status !== "offline" && m.user.id !== user)).then(r=>r.random(num));
     }
 
     static async GetDataForBodyguard(bot, toMember, roleInfo){
-        let spooked = await SpookRoles.#getEligibleUser(bot, toMember.guild);
+        let spooked = await SpookRoles.#getEligibleUser(bot, toMember.guild, 1, toMember.user.id);
         if(!spooked)return null;
         return {spooked: spooked.id};
     }
 
     static async GetDataForBully(bot, toMember, roleInfo){
-        let spooked = await SpookRoles.#getEligibleUser(bot, toMember.guild);
+        let spooked = await SpookRoles.#getEligibleUser(bot, toMember.guild,1, toMember.user.id);
         if(!spooked)return null;
         let num = bot.util.intBetween(2, 100);
         return {spooked: spooked.id, num};
     }
 
     static async GetDataForJoker(bot, toMember, roleInfo){
-        let [spooked, spooker] = await SpookRoles.#getEligibleUser(bot, toMember.guild, 2);
+        let [spooked, spooker] = await SpookRoles.#getEligibleUser(bot, toMember.guild, 2, toMember.user.id);
         if(!spooked || !spooker)return null;
         let num = bot.util.intBetween(2, 100);
         return {spooker: spooker.id, spooked: spooked.id, num};
