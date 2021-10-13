1391711029e68f25d59a300043e6fd73602ad7e9
Peter Maguire <me@petermaguire.xyz>
2021-10-13 20:41:31 +0100

    fix: fix disabled commands not really being disabled

--- util/middleware/CommandOverride.js
+++ util/middleware/CommandOverride.js
@@ -1,4 +1,10 @@
-module.exports = (context)=> {
+module.exports = (context, bot)=> {
+
+    if(context.getBool(`${context.command}.disable`)) {
+        bot.logger.log(`${context.command} is disabled in this server.`)
+        return false;
+    }
+
     if (context.getSetting(`${context.command}.override`)) {
         context.send(context.getSetting(`${context.command}.override`));
         return false;
