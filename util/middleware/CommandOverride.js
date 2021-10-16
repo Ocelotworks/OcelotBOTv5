c164727242e4f673e13300a83b7396dc2273ddc0
Peter Maguire <me@petermaguire.xyz>
2021-10-16 16:48:54 +0100

    feat: allow overriding subcommands

--- util/middleware/CommandOverride.js
+++ util/middleware/CommandOverride.js
@@ -5,8 +5,19 @@ module.exports = (context, bot)=> {
         return false;
     }
 
+    if(context.options.command && context.getBool(`${context.command}.${context.options.command}.disable`)){
+        bot.logger.log(`${context.command} ${context.options.command} is disabled in this server.`)
+        context.options.command = "help";
+        return true;
+    }
+
     if (context.getSetting(`${context.command}.override`)) {
         context.send(context.getSetting(`${context.command}.override`));
+        return false;
+    }
+
+    if(context.options.command && context.getSetting(`${context.command}.${context.options.command}.override`)){
+        context.send(context.getSetting(`${context.command}.${context.options.command}.override`));
         return false;
     }
     return true;
