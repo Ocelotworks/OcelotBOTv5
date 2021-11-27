56c775683e5101d9589cbcffb9ccc000cd090685
Peter Maguire <me@petermaguire.xyz>
2021-11-27 20:35:29 +0000

    fix(music): fix !music remove with no index trying to display the entire queue regardless of length (Sentry #OCELOTBOT-2Y1)

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -20,27 +20,28 @@ module.exports = {
 
 
         if (!context.options.pos) {
+            if(listener.queue.length > 10){
+                return context.send(`You must enter a track number to remove. Check **${context.getSetting("prefix")}music list** to find the track number.`);
+            }
             let output = `\`\`\`asciidoc\nEnter ${context.command} ${context.options.command} and then a number below:\n============\n`;
             for (let i = 0; i < listener.queue.length; i++) {
                 output += `${i + 1} :: ${listener.queue[i].info.title}\n`;
             }
             output += "\n```";
-            context.send(output);
-        } else {
-            let pos = context.options.pos-1
-            if (!listener.queue[pos])
-                return context.send({
-                    content: `:warning: There is no item at that position, retrieve the number from **${context.command} list**`,
-                    components: [bot.util.actionRow(bot.interactions.suggestedCommand(context, "list"))]
-                });
-
-            if (listener.voiceChannel.members.size > 2 && listener.queue[pos].requester !== context.user.id)
-                return context.send(`:bangbang: Only the person who requested the song (<@${listener.queue[pos].requester}>) can remove this song.`);
-
-            await context.send(`:white_check_mark: Removed **${listener.queue[pos].info.title}**`);
-            await bot.database.removeSong(listener.queue[pos].id);
-            listener.queue.splice(pos, 1);
+            return context.send(output);
         }
+        let pos = context.options.pos-1
+        if (!listener.queue[pos])
+            return context.send({
+                content: `:warning: There is no item at that position, retrieve the number from **${context.command} list**`,
+                components: [bot.util.actionRow(bot.interactions.suggestedCommand(context, "list"))]
+            });
 
+        if (listener.voiceChannel.members.size > 2 && listener.queue[pos].requester !== context.user.id)
+            return context.send(`:bangbang: Only the person who requested the song (<@${listener.queue[pos].requester}>) can remove this song.`);
+
+        await context.send(`:white_check_mark: Removed **${listener.queue[pos].info.title}**`);
+        await bot.database.removeSong(listener.queue[pos].id);
+        listener.queue.splice(pos, 1);
     }
 };
