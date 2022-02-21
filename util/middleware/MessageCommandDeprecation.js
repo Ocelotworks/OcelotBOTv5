c1a10ba4686ad9bda27bddf64b26faca1b2064cb
Peter Maguire <me@petermaguire.xyz>
2022-02-21 22:04:41 +0000

    add timeout to only show deprecation message every 2 hours

--- util/middleware/MessageCommandDeprecation.js
+++ util/middleware/MessageCommandDeprecation.js
@@ -1,5 +1,10 @@
+let seenMessage = {};
 module.exports = (context, bot) => {
-    if (!context.getBool("messageCommandDeprecation") || context.interaction || !context.message)return true;
+    if(!context.getBool("messageCommandDeprecation") || context.interaction || !context.message)return true;
+    const key = context.interaction || !context.message;
+    seenMessage[key] = seenMessage[key]+1 || 0;
+    if (seenMessage[key] && seenMessage[key] < 50)return true;
+    seenMessage[key] = 0;
     context.appendResponsePrefix(context.getLang("MESSAGE_COMMAND_DEPRECATION"));
     return true;
 }
