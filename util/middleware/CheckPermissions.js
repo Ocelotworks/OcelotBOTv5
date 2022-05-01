785e2ef53b990c9409fe0122ac71b819148f566a
Peter Maguire <me@petermaguire.xyz>
2022-05-01 22:41:07 +0100

    fix: i have not the heart to write a commit message (Sentry #OCELOTBOT-36G)

--- util/middleware/CheckPermissions.js
+++ util/middleware/CheckPermissions.js
@@ -32,7 +32,7 @@ module.exports = (context, bot) => {
     }
 
     // Check permissions in Guilds
-    if(context.member && userPermissions && !context.channel.permissionsFor(context.member).has(userPermissions)){
+    if(context.member && userPermissions && !context.channel.permissionsFor(context.member)?.has(userPermissions)){
         context.replyLang({content: "GENERIC_USER_PERMISSIONS", ephemeral: true}, {permissions: userPermissions.map((p)=>Strings.Permissions[p]).join(", ")})
         return false
     }
