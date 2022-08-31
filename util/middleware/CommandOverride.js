7f820594e341e9bf92bae8f74b863244e8e01b75
Peter Maguire <me@petermaguire.xyz>
2022-08-31 20:30:42 +0100

    fix: refactor command override system and add individual command notices

--- util/middleware/CommandOverride.js
+++ util/middleware/CommandOverride.js
@@ -1,24 +1,39 @@
 module.exports = (context, bot)=> {
-
-    if(context.getBool(`${context.command}.disable`)) {
-        bot.logger.log(`${context.command} is disabled in this server.`)
+    if(getCommandScopedBool(context, "override")) {
+        bot.logger.log(`${context.command} ${context.options.command || ""} is disabled in this server.`)
+        if(context.options.command){
+            context.options.command = "help";
+            return true;
+        }
         return false;
     }
 
-    if(context.options.command && context.getBool(`${context.command}.${context.options.command}.disable`)){
-        bot.logger.log(`${context.command} ${context.options.command} is disabled in this server.`)
-        context.options.command = "help";
+    if (getCommandScopedSetting(context, "override")) {
+        context.send(getCommandScopedSetting(context, "override"));
+        return false;
+    }
+
+    if (getCommandScopedSetting(context, "notice")) {
+        context.appendResponsePrefix(getCommandScopedSetting(context, "notice"));
         return true;
     }
 
-    if (context.getSetting(`${context.command}.override`)) {
-        context.send(context.getSetting(`${context.command}.override`));
-        return false;
-    }
-
-    if(context.options.command && context.getSetting(`${context.command}.${context.options.command}.override`)){
-        context.send(context.getSetting(`${context.command}.${context.options.command}.override`));
-        return false;
-    }
     return true;
 };
+
+function getCommandScope(context){
+    let scopedCommand = context.command;
+    if(context.options.command)
+        scopedCommand += "."+context.options.command;
+    return scopedCommand;
+}
+
+function getCommandScopedSetting(context, setting){
+    const scopedCommand = getCommandScope(context);
+    return context.getSetting(`${scopedCommand}.${setting}`) || context.getSetting(`${context.command}.${setting}`);
+}
+
+function getCommandScopedBool(context, setting){
+    const scopedCommand = getCommandScope(context);
+    return context.getBool(`${scopedCommand}.${setting}`) || context.getBool(`${context.command}.${setting}`);
+}
