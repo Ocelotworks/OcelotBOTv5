fb4b3a2c943875a12370987e80dccb1ff625fbf3
Peter Maguire <me@petermaguire.xyz>
2022-09-19 12:44:56 +0100

    feat(subs): add autocomplete to /subscriptions remove

--- commands/subscriptions/remove.js
+++ commands/subscriptions/remove.js
@@ -4,10 +4,20 @@
  * ╚════ ║   (ocelotbotv5) remove
  *  ════╝
  */
+const Strings = require("../../util/String");
 module.exports = {
     name: "Remove Sub",
     usage: "remove :0id",
     commands: ["remove", "delete"],
+    argDescriptions: {
+        id: {name: "The subscription ID to remove", autocomplete: true}
+    },
+    autocomplete: async function(input, interaction, bot) {
+        const subs = await bot.database.getSubscriptionsForChannel(interaction.channel.id, bot.client.user.id);
+        input = input ? input.toLowerCase() : "";
+        return subs.filter((s)=>s.id.toString().startsWith(input) || s.data.startsWith(input) || s.type.startsWith(input)).slice(0, 25).map((s)=>({name: Strings.Truncate(`(${s.id}) ${s.type} ${s.data}`, 100), value: s.id}))
+    },
+
     run:  async function(context, bot){
         const removed = await bot.database.removeSubscription(context.guild.id, context.channel.id, context.options.id);
         if(removed === 0)
