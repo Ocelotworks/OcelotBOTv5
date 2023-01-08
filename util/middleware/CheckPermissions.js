5cab3da4e8be574504898fd2dbb4a0b36a47129e
Peter Maguire <me@petermaguire.xyz>
2023-01-08 11:58:41 +0000

    fix: add setting to ignore permissions for specific commands

--- util/middleware/CheckPermissions.js
+++ util/middleware/CheckPermissions.js
@@ -31,6 +31,9 @@ module.exports = (context, bot) => {
         return false
     }
 
+    if(context.getBool(`${context.command}.ignorePermissions`))
+        return true
+
     // Check permissions in Guilds
     if(context.member && userPermissions && !context.channel.permissionsFor(context.member)?.has(userPermissions)){
         context.replyLang({content: "GENERIC_USER_PERMISSIONS", ephemeral: true}, {permissions: userPermissions.map((p)=>Strings.Permissions[p]).join(", ")})
