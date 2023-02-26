7c038f6e64902dbd8c91323381989110a68edd74
Peter Maguire <me@petermaguire.xyz>
2023-02-26 11:00:31 +0000

    fix(admin): ban error when a @mention is passed in

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -12,11 +12,13 @@ module.exports = {
     noCustom: true,
     slashHidden: true,
     run: async function (context, bot) {
-        const target = Strings.GetUserFromMention(bot, context.options.user) || context.options.user;
+        const target = Strings.GetUserFromMention(bot, context.options.user)?.id || context.options.user;
+        if(!target){
+            return context.send("Couldn't find user, try using their ID");
+        }
         await bot.database.ban(target, "user", context.options.reason || "Admin ban");
         await bot.rabbit.event({type: "updateBans"});
         bot.banCache.user.push(target);
-        message.channel.send(`<@${target}> has been banned!`);
-
+        context.send(`<@${target}> has been banned!`);
     }
 };
